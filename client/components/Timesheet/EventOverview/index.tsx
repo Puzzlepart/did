91d6961a0a1ac0855d8c23f40b31c4506734245f
olemp <olemp@puzzlepart.com>
2020-02-12 13:00:43 +0100

    Initial commit

--- client/components/Timesheet/EventOverview/index.tsx
+++ client/components/Timesheet/EventOverview/index.tsx
@@ -45,7 +45,7 @@ const CreateColumns = (period: ITimesheetPeriod) => {
     return [
         col('label', '', { minWidth: 50, maxWidth: 270, isMultiline: true }, (row: any) => <LabelColumn row={row} />),
         ...Array.from(Array(7).keys()).map(i => {
-            const day = startOfWeek(undefined, undefined, period.startDateTime).add(i as moment.DurationInputArg1, 'days' as moment.DurationInputArg2);
+            const day = startOfWeek(period.startDateTime).add(i as moment.DurationInputArg1, 'days' as moment.DurationInputArg2);
             return col(day.format('L'), day.format('ddd Do'), { maxWidth: 70, minWidth: 70 });
         }),
         col('sum', 'Sum')
