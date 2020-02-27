c4b41dbedac035b3d5b2ce739f55d0fc6506e384
olemp <olemp@puzzlepart.com>
2020-02-27 13:06:04 +0100

    Renders event errors

--- client/components/Timesheet/EventList/ProjectColumn/index.tsx
+++ client/components/Timesheet/EventList/ProjectColumn/index.tsx
@@ -6,9 +6,8 @@ import { Icon } from 'office-ui-fabric-react/lib/Icon';
 import { MessageBarType } from 'office-ui-fabric-react/lib/MessageBar';
 import * as React from 'react';
 import { useState } from 'react';
-import { ResolveProjectModal } from './ResolveProjectModal';
-import { TooltipHost } from 'office-ui-fabric-react/lib/Tooltip';
 import { IProjectColumnProps } from './IProjectColumnProps';
+import { ResolveProjectModal } from './ResolveProjectModal';
 
 /**
  * @component ProjectColumn
@@ -20,18 +19,32 @@ export const ProjectColumn = ({ event, isConfirmed, onProjectSelected, onProject
 
     if (!event.project) {
         if (isConfirmed) return null;
+        if (event.error) {
+            return (
+                <div className='c-Timesheet-projectColumn'>
+                    <UserMessage
+                        style={{ marginTop: 10 }}
+                        isMultiline={false}
+                        type={MessageBarType.severeWarning}
+                        iconName='Warning'
+                        text={`**NOTE:** ${event.error.message}`} />
+                </div>
+            );
+        }
         return (
             <div className='c-Timesheet-projectColumn'>
                 <UserMessage
-                    style={{ width: 260, marginTop: 10 }}
+                    style={{ marginTop: 10 }}
+                    isMultiline={false}
                     type={MessageBarType.warning}
                     iconName='TagUnknown'
+                    text='No match found'
                     actions={
                         <div>
                             <MessageBarButton onClick={_ => setModal(true)} id={toggleId}>Resolve</MessageBarButton>
                             <MessageBarButton onClick={onProjectIgnore}>Ignore</MessageBarButton>
                         </div>
-                    }>Not matched</UserMessage>
+                    } />
                 <ResolveProjectModal
                     event={event}
                     onDismiss={() => setModal(false)}
@@ -44,19 +57,13 @@ export const ProjectColumn = ({ event, isConfirmed, onProjectSelected, onProject
         );
     }
 
-
     return (
         <div className='c-Timesheet-projectColumn'>
-            <div className='c-Timesheet-projectColumn-warning'>
-                <TooltipHost
-                    content='The project or customer is no longer valid.'
-                    styles={{ root: { display: 'inline-block' } }}>
-                    <Icon iconName='Warning' styles={{ root: { fontSize: 14, color: 'red' } }} />
-                </TooltipHost>
-            </div>
             <div className='c-Timesheet-projectColumn-content'>
                 <div><a href={`/projects#${event.project.id}`}>{event.project.name}</a></div>
-                <div style={{ fontSize: '7pt' }}>for <a style={{ fontSize: '7pt' }} href={`/customers#${event.customer.id}`}>{event.customer.name}</a></div>
+                <div style={{ fontSize: '7pt' }}>
+                    for <a style={{ fontSize: '7pt' }} href={`/customers#${event.customer.id}`}>{event.customer.name}</a>
+                </div>
             </div>
             <div className='c-Timesheet-projectColumn-clear' title='Clear' hidden={!event.isManualMatch}>
                 <span onClick={onProjectClear} style={{ cursor: 'pointer' }}><Icon iconName='Cancel' styles={{ root: { fontSize: 14 } }} /></span>
