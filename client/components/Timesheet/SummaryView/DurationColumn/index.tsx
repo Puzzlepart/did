efdac6d5d9c11363270390133a59cf2136b8dcae
olemp <olemp@puzzlepart.com>
2020-02-28 16:09:17 +0100

    Misc

--- client/components/Timesheet/SummaryView/DurationColumn/index.tsx
+++ client/components/Timesheet/SummaryView/DurationColumn/index.tsx
@@ -11,8 +11,8 @@ export interface IDurationColumnProps {
  * @description 
  */
 export const DurationColumn = ({ row, column }: IDurationColumnProps) => {
-    let style: React.CSSProperties = column.data.style || {};
-    if (row.label === 'Total' || column.fieldName === 'Sum') style.fontWeight = 500;
+    let style: React.CSSProperties = { ...column.data.style };
+    if (row.label === 'Total') style.fontWeight = 500;
     let value = row[column.fieldName] ? Number.parseFloat(row[column.fieldName]).toFixed(2) : null;
     return (
         <div style={style}>
