b02338ba79bd8ef779fbd5fa6c38990ac0979d12
olemp <olemp@puzzlepart.com>
2020-03-02 17:04:44 +0100

    Added preview to LabelFormModal

--- client/components/AdminView/Labels/LabelFormModal/index.tsx
+++ client/components/AdminView/Labels/LabelFormModal/index.tsx
@@ -1,6 +1,8 @@
 import { useMutation } from '@apollo/react-hooks';
+import { EntityLabel } from 'components/EntityLabel';
 import { ILabel } from 'interfaces';
 import { PrimaryButton } from 'office-ui-fabric-react/lib/Button';
+import { Label } from 'office-ui-fabric-react/lib/Label';
 import { Modal } from 'office-ui-fabric-react/lib/Modal';
 import { TextField } from 'office-ui-fabric-react/lib/TextField';
 import * as React from 'react';
@@ -25,7 +27,10 @@ export const LabelFormModal = (props: ILabelFormModalProps) => {
         props.modal.onDismiss();
     }
 
-    const isFormValid = () => {
+    /**
+     * Is form valid
+     */
+    const isFormValid = (): boolean => {
         return !validator.isEmpty(label.name) && !validator.isEmpty(label.description) && !validator.isEmpty(label.color);
     }
 
@@ -39,17 +44,20 @@ export const LabelFormModal = (props: ILabelFormModalProps) => {
                 label='Name'
                 value={label.name}
                 required={true}
-                onChange={(_, id) => setLabel({ ...label, name })} />
+                onChange={(_, name) => setLabel({ ...label, name })} />
             <TextField
                 label='Description'
                 value={label.description}
                 required={true}
+                multiline={true}
                 onChange={(_, description) => setLabel({ ...label, description })} />
             <TextField
                 label='Color'
                 value={label.color}
                 required={true}
                 onChange={(_, color) => setLabel({ ...label, color })} />
+            <Label>Preview</Label>
+            <EntityLabel {...label} />
             <PrimaryButton
                 className='c-AdminView-labelFormModal-saveBtn'
                 text='Save'
