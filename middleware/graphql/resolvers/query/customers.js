6f35c2acaebb3fc5b8aab95355d1162e81f410c7
olemp <olemp@puzzlepart.com>
2020-03-02 16:04:47 +0100

    Projects and customers included labels in graphql

--- middleware/graphql/resolvers/query/customers.js
+++ middleware/graphql/resolvers/query/customers.js
@@ -9,7 +9,14 @@ const log = require('debug')('middleware/graphql/resolvers/query/customers');
  */
 async function customers(_obj, _args, context) {
     log('Retrieving customers from storage');
-    let customers = await context.services.storage.getCustomers();
+    let [customers, labels] = await Promise.all([
+        context.services.storage.getCustomers(),
+        context.services.storage.getLabels(),
+    ]);
+    customers = customers.map(customer => ({
+        ...customer,
+        labels: _.filter(labels, l => (customer.labels || '').indexOf(l.id) !== -1),
+    }));
     log('Retrieved %s customers from storage', customers.length);
     return customers;
 };
--- middleware/graphql/resolvers/query/projects.js
+++ middleware/graphql/resolvers/query/projects.js
@@ -10,14 +10,16 @@ const log = require('debug')('middleware/graphql/resolvers/query/getProjects');
  */
 async function projects(_obj, args, context) {
     log('Retrieving projects from storage. customerKey: %s, sortBy: %s', args.customerKey, args.sortBy);
-    let [projects, customers] = await Promise.all([
+    let [projects, customers, labels] = await Promise.all([
         context.services.storage.getProjects(args.customerKey, args.sortBy),
         context.services.storage.getCustomers(),
+        context.services.storage.getLabels(),
     ]);
     log('Retrieved %s projects from storage', projects.length);
-    projects = projects.map(p => ({
-        ...p,
-        customer: _.find(customers, c => c.id === p.id.split(' ')[0]),
+    projects = projects.map(project => ({
+        ...project,
+        customer: _.find(customers, c => c.id === project.id.split(' ')[0]),
+        labels: _.filter(labels, l => (project.labels || '').indexOf(l.id) !== -1),
     }));
     projects = projects.filter(p => p.customer);
     return projects;
