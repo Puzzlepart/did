52dd4cbe5d88bc894e87b36a4d51f223133bf3e6
olemp <olemp@puzzlepart.com>
2020-04-08 15:21:51 +0200

    Added more props to ITimesheetPeriod

--- client/components/Timesheet/ITimesheetPeriod.ts
+++ client/components/Timesheet/ITimesheetPeriod.ts
@@ -2,9 +2,12 @@ import { ITimeEntry } from 'interfaces';
 
 export interface ITimesheetPeriod {
   id: string;
-  name: string;
+  name: string;  
+  startDateTime?: string;
+  endDateTime?: string;
   events?: ITimeEntry[];
   totalDuration?: number;
   confirmedDuration?: number;
   isConfirmed?: boolean;
+  errors?: Error[];
 }
