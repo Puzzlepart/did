5c5e6992115c7c6533ecd7af1f750d6277b9caa3
olemp <olemp@puzzlepart.com>
2020-04-28 17:54:00 +0200

    Added client/components/index for easier import of several components at once

---
+++ client/components/index.tsx
@@ -0,0 +1,7 @@
+export * from './AdminView';
+export * from './Customers';
+export * from './Projects';
+export * from './Reports';
+export * from './Timesheet';
+import UserNotifications from './UserNotifications';
+export { UserNotifications };
