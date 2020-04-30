74a00cb40d2d1b3af83a7b326c9a97db50adfc2c
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-04-30 15:52:16 +0200

    Added todos to resolver for notifications

--- middleware/graphql/resolvers/query/notifications.js
+++ middleware/graphql/resolvers/query/notifications.js
@@ -24,6 +24,15 @@ const NOTIFICATION_SEVERITY = {
  * @param {*} context Context
  */
 async function notifications(_obj, _args, context) {
+    
+    
+
+    //TODO: Need to get weeks in month dynamically
+    // Start from (Current week number -1), traverse (n) 5? weeks
+    // Change to weeksToCheck etc
+    const weeksInMonth = [9, 10, 11, 12, 13, 14];
+    
+    // TODO startDateTime = start of week oldest week, end endDateTime = endOfWeek last week
     const startDateTime = startOfMonth(startOfMonth().subtract(1, 'month'));
     const endDateTime = endOfMonth(endOfMonth().subtract(1, 'month'));
     let [confirmedTimeEntries, notifications] = await Promise.all([
@@ -35,13 +44,11 @@ async function notifications(_obj, _args, context) {
         context.services.storage.getNotifications(),
     ])
 
-    //TODO: Need to get weeks in month dynamically
-    const weeksInMonth = [9, 10, 11, 12, 13, 14];
-
     const confirmedWeeks = _.unique(confirmedTimeEntries, entry => entry.weekNumber).map(entry => entry.weekNumber);
     const unconfirmedWeeks = _.difference(weeksInMonth, confirmedWeeks);
 
     //TODO: Adding unconfirmed weeks notifications
+    // TODO: i18n for text, need to move i18n to root
     notifications.push(...unconfirmedWeeks.map(week => ({
         id: uuid(),
         type: NOTIFICATION_TYPE.WEEK_NOT_CONFIRMED,
