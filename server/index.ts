b028c6a11f7e74378a537f008bd11af3afefdf19
olemp <olemp@puzzlepart.com>
2020-04-30 19:55:44 +0200

    Initial setup

---
+++ server/index.ts
@@ -0,0 +1,16 @@
+import { app } from './app';
+import http from 'http';
+var port = process.env.PORT || '8080';
+app.set('port', port);
+
+var server = http.createServer(app);
+
+server.listen(port);
+
+server.on('error', (error: Error) => {
+  throw error;
+});
+
+server.on('listening', () => {
+  console.log('\x1b[32m', `[Server listening on port ${port}]`);
+});
