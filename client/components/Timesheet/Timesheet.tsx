f09cd4003ef74c411e941e47b17197c0e08d7ca7
olemp <olemp@puzzlepart.com>
2020-05-03 21:40:05 +0200

    Timesheet using reducer to keep state

--- client/components/Timesheet/Timesheet.tsx
+++ client/components/Timesheet/Timesheet.tsx
@@ -6,7 +6,7 @@ import * as helpers from 'helpers';
 import resource from 'i18n';
 import { IProject, ITimeEntry } from 'interfaces';
 import { Pivot, PivotItem } from 'office-ui-fabric-react/lib/Pivot';
-import { ProgressIndicator, IProgressIndicatorProps } from 'office-ui-fabric-react/lib/ProgressIndicator';
+import { ProgressIndicator } from 'office-ui-fabric-react/lib/ProgressIndicator';
 import * as React from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import _ from 'underscore';
@@ -15,19 +15,83 @@ import CONFIRM_PERIOD from './CONFIRM_PERIOD';
 import GET_TIMESHEET from './GET_TIMESHEET';
 import { StatusBar } from './StatusBar';
 import { SummaryView, SummaryViewType } from './SummaryView';
+import { TimesheetContext } from './TimesheetContext';
 import { TimesheetPeriod } from './TimesheetPeriod';
 import { TimesheetScope } from './TimesheetScope';
-import { TimesheetView } from './types';
+import { ITimesheetState, TimesheetView } from './types';
 import UNCONFIRM_PERIOD from './UNCONFIRM_PERIOD';
-import { TimesheetContext } from './TimesheetContext';
+
+function reducer(state: ITimesheetState, action: { type: string; payload: any }): any {
+    // eslint-disable-next-line prefer-const
+    let newState = { ...state };
+    switch (action.type) {
+        case 'DATA_UPDATED': {
+            newState.loading = action.payload.loading ? { label: 'Loading events', description: 'Please wait...' } : null
+            if (action.payload.data) {
+                newState.periods = action.payload.data.timesheet.map(period => new TimesheetPeriod(period));
+            }
+        }
+            break;
+        case 'CONFIRMING_PERIOD': {
+            newState.loading = { label: 'Confirming period', description: 'Hang on a minute...' };
+        }
+            break;
+        case 'UNCONFIRMING_PERIOD': {
+            newState.loading = { label: 'Unconfirming period', description: 'Hang on a minute...' };
+        }
+            break;
+        case 'UPDATE_SCOPE': {
+            newState.scope = new TimesheetScope(action.payload);
+        }
+            break;
+        case 'CHANGE_PERIOD': {
+            newState.selectedPeriodId = action.payload;
+        }
+            break;
+        case 'MANUAL_MATCH': {
+            const { selectedPeriod, event, project } = action.payload;
+            selectedPeriod.setManualMatch(event.id, project);
+            newState.periods = newState.periods.map(p => p.id === newState.selectedPeriodId ? selectedPeriod : p);
+        }
+            break;
+        case 'CLEAR_MANUAL_MATCH': {
+            const { selectedPeriod, event } = action.payload;
+            selectedPeriod.clearManualMatch(event.id);
+            newState.periods = newState.periods.map(p => p.id === newState.selectedPeriodId ? selectedPeriod : p);
+        }
+            break;
+        case 'IGNORE_EVENT': {
+            const { selectedPeriod, event } = action.payload;
+            selectedPeriod.ignoreEvent(event.id);
+            newState.periods = newState.periods.map(p => p.id === newState.selectedPeriodId ? selectedPeriod : p);
+        }
+            break;
+        case 'CLEAR_IGNORES': {
+            const { selectedPeriod } = action.payload;
+            selectedPeriod.clearIgnoredEvents();
+            newState.periods = newState.periods.map(p => p.id === newState.selectedPeriodId ? selectedPeriod : p);
+        }
+            break;
+        default: throw new Error();
+    }
+    if (newState.periods.length > 0) {
+        newState.selectedPeriod = _.find(newState.periods, (p: TimesheetPeriod) => p.id === newState.selectedPeriodId) || _.first(newState.periods);
+    }
+    return newState;
+}
+
+const intialState = {
+    loading: null,
+    periods: [],
+    selectedPeriodId: null,
+    selectedPeriod: new TimesheetPeriod(),
+    scope: new TimesheetScope(),
+};
 
 export const Timesheet = () => {
     const history = useHistory();
     const params = useParams<{ startDateTime: string; view: TimesheetView }>();
-    const [loading, setLoading] = React.useState<IProgressIndicatorProps>({});
-    const [scope, setScope] = React.useState<TimesheetScope>(new TimesheetScope());
-    const [periods, setPeriods] = React.useState<TimesheetPeriod[]>([]);
-    const [selectedPeriodId, setSelectedPeriodId] = React.useState<string>();
+    const [{ loading, periods, selectedPeriod, scope }, dispatch]: any = React.useReducer<any>(reducer, intialState);
     const timesheetQuery = useQuery<{ timesheet: TimesheetPeriod[] }>(GET_TIMESHEET, {
         variables: {
             ...scope.iso,
@@ -39,52 +103,32 @@ export const Timesheet = () => {
     const [confirmPeriod] = useMutation<{ entries: any[]; startDateTime: string; endDateTime: string }>(CONFIRM_PERIOD);
     const [unconfirmPeriod] = useMutation<{ startDateTime: string; endDateTime: string }>(UNCONFIRM_PERIOD);
 
-    React.useEffect(() => {
-        setLoading(timesheetQuery.loading ? { label: 'Loading events', description: 'Please wait...' } : null);
-        if (timesheetQuery.data) setPeriods(timesheetQuery.data.timesheet.map(period => new TimesheetPeriod(period)));
-    }, [timesheetQuery]);
 
-    const selectedPeriod = React.useMemo(() => _.find(periods, p => p.id === selectedPeriodId) || _.first(periods) || new TimesheetPeriod(), [periods, selectedPeriodId]);
-
-    React.useEffect(() => setScope(new TimesheetScope(params.startDateTime)), [params.startDateTime]);
+    React.useEffect(() => dispatch({ type: 'DATA_UPDATED', payload: timesheetQuery }), [timesheetQuery])
+    React.useEffect(() => dispatch({ type: 'UPDATE_SCOPE', payload: params.startDateTime }), [params.startDateTime]);
 
     const onConfirmPeriod = async () => {
-        setLoading({ label: 'Confirming period', description: 'Hang on a minute...' });
+        dispatch({ type: 'CONFIRMING_PERIOD' });
         await confirmPeriod({ variables: { ...selectedPeriod.scope, entries: selectedPeriod.matchedEvents } });
         timesheetQuery.refetch();
     }
 
     const onUnconfirmPeriod = async () => {
-        setLoading({ label: 'Unconfirming period', description: 'Hang on a minute...' });
+        dispatch({ type: 'UNCONFIRMING_PERIOD' });
         await unconfirmPeriod({ variables: selectedPeriod.scope });
         timesheetQuery.refetch();
     }
 
-    const onManualMatch = (event: ITimeEntry, project: IProject) => {
-        selectedPeriod.setManualMatch(event.id, project);
-        setPeriods([...periods].map(p => p.id === selectedPeriodId ? selectedPeriod : p));
-    }
-
-    const onClearManualMatch = (event: ITimeEntry) => {
-        selectedPeriod.clearManualMatch(event.id);
-        setPeriods([...periods].map(p => p.id === selectedPeriodId ? selectedPeriod : p));
-    }
-
-    const onIgnoreEvent = (event: ITimeEntry) => {
-        selectedPeriod.ignoreEvent(event.id);
-        setPeriods([...periods].map(p => p.id === selectedPeriodId ? selectedPeriod : p));
-    }
-
-    const onClearIgnores = () => {
-        selectedPeriod.clearIgnoredEvents();
-        setPeriods([...periods].map(p => p.id === selectedPeriodId ? selectedPeriod : p));
-    }
+    const onManualMatch = (event: ITimeEntry, project: IProject) => dispatch({ type: 'MANUAL_MATCH', payload: { selectedPeriod, event, project } });
+    const onClearManualMatch = (event: ITimeEntry) => dispatch({ type: 'CLEAR_MANUAL_MATCH', payload: { selectedPeriod, event } });
+    const onIgnoreEvent = (event: ITimeEntry) => dispatch({ type: 'IGNORE_EVENT', payload: { selectedPeriod, event } });
+    const onClearIgnores = () => dispatch({ type: 'CLEAR_IGNORES', payload: { selectedPeriod } });
 
     return (
         <TimesheetContext.Provider value={{ periods, selectedPeriod, scope, loading: !!loading }}>
             <div className='c-Timesheet'>
                 <ActionBar
-                    onChangePeriod={periodId => setSelectedPeriodId(periodId)}
+                    onChangePeriod={periodId => dispatch({ type: 'CHANGE_PERIOD', payload: periodId })}
                     onConfirmPeriod={onConfirmPeriod}
                     onUnconfirmPeriod={onUnconfirmPeriod} />
                 <Pivot defaultSelectedKey={params.view} onLinkClick={item => history.push(`/timesheet/${item.props.itemKey}/${scope.iso.startDateTime}`)}>
--- client/components/Timesheet/types.ts
+++ client/components/Timesheet/types.ts
@@ -1,4 +1,19 @@
+import { IProgressIndicatorProps } from 'office-ui-fabric-react/lib/ProgressIndicator';
+import { TimesheetPeriod } from './TimesheetPeriod';
+import { TimesheetScope } from './TimesheetScope';
+
 /**
  * @category Timesheet
  */
 export type TimesheetView = 'overview' | 'summary' | 'allocation';
+
+/**
+ * @category Timesheet
+ */
+export interface ITimesheetState {
+    loading: IProgressIndicatorProps;
+    periods: TimesheetPeriod[];
+    selectedPeriodId: string;
+    selectedPeriod: TimesheetPeriod;
+    scope: TimesheetScope;
+}
