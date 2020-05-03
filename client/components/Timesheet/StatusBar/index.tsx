0a8ebc04c055fd8687b805456bc76ad67e1416f2
olemp <olemp@puzzlepart.com>
2020-05-04 00:49:31 +0200

    Cleaned up JSX markup in StatusBar

--- client/components/Timesheet/StatusBar/index.tsx
+++ client/components/Timesheet/StatusBar/index.tsx
@@ -20,49 +20,52 @@ export const StatusBar = () => {
             {!loading && (
                 <div className='container'>
                     <div className='row'>
-                        <div className='col-sm'
-                            hidden={selectedPeriod.isConfirmed}>
-                            <UserMessage text={format(resource('TIMESHEET.PERIOD_HOURS_SUMMARY_TEXT'), getDurationDisplay(selectedPeriod.totalDuration))} iconName='ReminderTime' />
-                        </div>
-                        <div className='col-sm' hidden={selectedPeriod.unmatchedDuration === 0 || selectedPeriod.isConfirmed}>
-                            <UserMessage
-                                text={format(resource('TIMESHEET.HOURS_NOT_MATCHED_TEXT'), getDurationDisplay(selectedPeriod.unmatchedDuration))}
-                                type={MessageBarType.warning}
-                                iconName='BufferTimeBoth' />
-                        </div>
-                        <div className='col-sm' hidden={selectedPeriod.unmatchedDuration > 0 || selectedPeriod.isConfirmed}>
-                            <UserMessage
-                                text={resource('TIMESHEET.ALL_HOURS_MATCHED_TEXT')}
-                                type={MessageBarType.success}
-                                iconName='BufferTimeBoth' />
-                        </div>
-                        <div className='col-sm' hidden={!selectedPeriod.isConfirmed}>
-                            <UserMessage
-                                text={format(resource('TIMESHEET.PERIOD_CONFIRMED_TEXT'), getDurationDisplay(selectedPeriod.matchedDuration))}
-                                type={MessageBarType.success}
-                                iconName='CheckMark' />
-                        </div>
-                        <div className='col-sm' hidden={selectedPeriod.ignoredEvents.length === 0 || selectedPeriod.isConfirmed}>
-                            <UserMessage
-                                type={MessageBarType.info}
-                                iconName='DependencyRemove'>
-                                <p>{format(resource('TIMESHEET.IGNORED_EVENTS_TEXT'), selectedPeriod.ignoredEvents.length)} <a href='#' onClick={() => dispatch({ type: 'CLEAR_IGNORES' })}>{resource('TIMESHEET.UNDO_IGNORE_LINK_TEXT')}</a></p>
-                            </UserMessage>
-                        </div>
-                        <div className='col-sm' hidden={selectedPeriod.errors.length === 0}>
-                            <UserMessage
-                                type={MessageBarType.severeWarning}
-                                iconName='ErrorBadge'>
-                                <p>{format(resource('TIMESHEET.UNRESOLVER_ERRORS_TEXT'), selectedPeriod.errors.length)}</p>
-                            </UserMessage>
-                        </div>
-                        <div className='col-sm' hidden={periods.length < 2}>
-                            <UserMessage
-                                type={MessageBarType.info}
-                                iconName='SplitObject'>
-                                <p>{resource('TIMESHEET.SPLIT_WEEK_TEXT')}</p>
-                            </UserMessage>
-                        </div>
+                        <UserMessage
+                            className='col-sm'
+                            hidden={selectedPeriod.isConfirmed}
+                            text={format(resource('TIMESHEET.PERIOD_HOURS_SUMMARY_TEXT'), getDurationDisplay(selectedPeriod.totalDuration))}
+                            iconName='ReminderTime' />
+
+                        <UserMessage
+                            className='col-sm' hidden={selectedPeriod.unmatchedDuration === 0 || selectedPeriod.isConfirmed}
+                            text={format(resource('TIMESHEET.HOURS_NOT_MATCHED_TEXT'), getDurationDisplay(selectedPeriod.unmatchedDuration))}
+                            type={MessageBarType.warning}
+                            iconName='BufferTimeBoth' />
+
+                        <UserMessage
+                            className='col-sm'
+                            hidden={selectedPeriod.unmatchedDuration > 0 || selectedPeriod.isConfirmed}
+                            text={resource('TIMESHEET.ALL_HOURS_MATCHED_TEXT')}
+                            type={MessageBarType.success}
+                            iconName='BufferTimeBoth' />
+
+                        <UserMessage
+                            className='col-sm' hidden={!selectedPeriod.isConfirmed}
+                            text={format(resource('TIMESHEET.PERIOD_CONFIRMED_TEXT'), getDurationDisplay(selectedPeriod.matchedDuration))}
+                            type={MessageBarType.success}
+                            iconName='CheckMark' />
+
+                        <UserMessage
+                            className='col-sm'
+                            hidden={selectedPeriod.ignoredEvents.length === 0 || selectedPeriod.isConfirmed}
+                            iconName='DependencyRemove'>
+                            <p>{format(resource('TIMESHEET.IGNORED_EVENTS_TEXT'), selectedPeriod.ignoredEvents.length)} <a href='#' onClick={() => dispatch({ type: 'CLEAR_IGNORES' })}>{resource('TIMESHEET.UNDO_IGNORE_LINK_TEXT')}</a></p>
+                        </UserMessage>
+
+                        <UserMessage
+                            className='col-sm'
+                            hidden={selectedPeriod.errors.length === 0}
+                            type={MessageBarType.severeWarning}
+                            iconName='ErrorBadge'>
+                            <p>{format(resource('TIMESHEET.UNRESOLVER_ERRORS_TEXT'), selectedPeriod.errors.length)}</p>
+                        </UserMessage>
+
+                        <UserMessage
+                            className='col-sm'
+                            hidden={periods.length < 2}
+                            iconName='SplitObject'>
+                            <p>{resource('TIMESHEET.SPLIT_WEEK_TEXT')}</p>
+                        </UserMessage>
                     </div>
                 </div>
             )}
