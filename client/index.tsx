f11397b4607e80f5ecbe8e5e999a7048201df984
olemp <olemp@puzzlepart.com>
2020-05-04 16:27:59 +0200

    Fixed i18n imports

--- client/index.tsx
+++ client/index.tsx
@@ -1,23 +1,26 @@
 
 import { initializeIcons } from '@uifabric/icons';
+import { IAppContext } from 'AppContext';
 import 'core-js/stable';
 import * as React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import * as ReactDom from 'react-dom';
 import 'regenerator-runtime/runtime.js';
+import { tryParseJson } from 'utils/tryParseJson';
 import { App } from './app';
 import { client } from './graphql';
 import * as i18n from './i18n';
-import { tryParseJson } from 'utils/tryParseJson';
 
 (async () => {
     initializeIcons();
-    await i18n.setup({ en: require('../../resources/en.json') });
+    await i18n.setup({ en: require('../resources/en.json') });
 
     const container = document.getElementById('app');
 
-    const context = tryParseJson(container.attributes.getNamedItem('data-props').value, { user: {} });
+    const context = tryParseJson<IAppContext>(container.attributes.getNamedItem('data-props').value, { user: {} });
     container.attributes.removeNamedItem('data-props');
+
+    context.user.userLanguage = 'nb_no';
 
     ReactDom.render((
         <ApolloProvider client={client}>
