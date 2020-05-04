48ee0caeaccbcada3574353571ae69676706fc91
olemp <olemp@puzzlepart.com>
2020-05-04 17:39:26 +0200

    Implemented sass-modules like SPFx, converted ProjectDetails for now

--- webpack.config.js
+++ webpack.config.js
@@ -39,38 +39,14 @@ let config = {
         ]
       },
       {
-        test: /\.module\.s(a|c)ss$/,
-        loader: [
-          mode === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              sourceMap: mode === 'development'
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: mode === 'development'
-            }
-          }
+        test: /\.scss$/, use: [
+          { loader: "style-loader" },  // to inject the result into the DOM as a style block
+          { loader: "css-modules-typescript-loader" },  // to generate a .d.ts module next to the .scss file (also requires a declaration.d.ts with "declare modules '*.scss';" in it to tell TypeScript that "import styles from './styles.scss';" means to load the module "./styles.scss.d.td")
+          { loader: "css-loader", options: { modules: true } },  // to convert the resulting CSS to Javascript to be bundled (modules:true to rename CSS classes in output to cryptic identifiers, except if wrapped in a :global(...) pseudo class)
+          { loader: "sass-loader" },  // to convert SASS to CSS
+          // NOTE: The first build after adding/removing/renaming CSS classes fails, since the newly generated .d.ts typescript module is picked up only later
         ]
       },
-      {
-        test: /\.s(a|c)ss$/,
-        exclude: /\.module.(s(a|c)ss)$/,
-        loader: [
-          mode === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: mode === 'development'
-            }
-          }
-        ]
-      }
     ]
   },
   resolve: {
@@ -82,7 +58,7 @@ let config = {
       common: path.resolve(src, 'common'),
       i18n: path.resolve(src, 'i18n'),
     },
-    extensions: ['.ts', '.tsx', '.js']
+    extensions: [".ts", ".tsx", ".js", ".css", ".scss"]
   },
   plugins: [
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
@@ -100,7 +76,6 @@ let config = {
 
 switch (mode) {
   case 'development': {
-    config.watch = true;
     config.stats = 'errors-only';
   }
     break;
