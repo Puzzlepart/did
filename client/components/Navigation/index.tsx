56eaeb1fb60c02561ed79c7d78b6ec12020dfcfb
olemp <olemp@puzzlepart.com>
2020-05-08 01:51:59 +0200

    Minor edit to nav logo (a tad smaller)

--- client/components/Navigation/index.tsx
+++ client/components/Navigation/index.tsx
@@ -11,7 +11,7 @@ export const Navigation = () => {
     return (
         <nav className={styles.root}>
             <div className={styles.container}>
-                <Link to='/' className={styles.logo}>
+                <Link to='/' className={styles.logo} title='Did 365 - The Calendar is the Timesheet'>
                     <img src='/images/D_beta_sm.png' />
                 </Link>
                 <ul className={styles.nav} hidden={!user}>
