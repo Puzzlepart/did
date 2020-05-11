c17dae080523b843689039ab1c31a0375cc2459c
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-05-11 14:19:26 +0200

    #340 (#341)

--- deploy.cmd
+++ deploy.cmd
@@ -78,7 +78,7 @@ echo INSTALLING NPM PACKAGES
 
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install --force --no-progress
+  call :ExecuteCmd !NPM_CMD! install --no-progress --no-fund
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
