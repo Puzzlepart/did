4259a398a20945e00770b1d5e92e36c53f5cf635
olemp <olemp@puzzlepart.com>
2020-05-15 13:51:48 +0200

    Initial unit tests

---
+++ tests/index.js
@@ -0,0 +1,88 @@
+const assert = require('assert');
+const _ = require('underscore');
+const matchEvents = require('../middleware/graphql/resolvers/timesheet.matching');
+const { data: { projects } } = require('./data/projects.json');
+const { data: { customers } } = require('./data/customers.json');
+
+describe('[Event matching]', () => {
+    let testEvent = {};
+
+    beforeEach(() => {
+        testEvent = {
+            "title": "hello",
+            "categories": [],
+        };
+    })
+
+    describe('Match against project', () => {
+        it('IAM VAC in category should match against customer Employee Absence', () => {
+            testEvent.categories.push('IAM VAC');
+            const event = _.first(matchEvents([testEvent], projects, customers));
+            assert.strictEqual(event.customer.name, 'Employee Absence');
+        });
+
+
+        it('[IAM VAC] in subject should match against customer Employee Absence', () => {
+            testEvent.title = '[IAM VAC]';
+            const event = _.first(matchEvents([testEvent], projects, customers));
+            assert.strictEqual(event.customer.name, 'Employee Absence');
+        });
+
+        it('[IAM VAC] in body should match against customer Employee Absence', () => {
+            testEvent.body = 'Hello this is an event [IAM VAC]';
+            const event = _.first(matchEvents([testEvent], projects, customers));
+            assert.strictEqual(event.customer.name, 'Employee Absence');
+        });
+
+        it('{IAM VAC} in body should match against customer Employee Absence', () => {
+            testEvent.body = 'Hello this is an event {IAM VAC}';
+            const event = _.first(matchEvents([testEvent], projects, customers));
+            assert.strictEqual(event.customer.name, 'Employee Absence');
+        });
+
+        it('/IAM VAC/ in body should match against customer Employee Absence', () => {
+            testEvent.body = 'Hello this is an event /IAM VAC/';
+            const event = _.first(matchEvents([testEvent], projects, customers));
+            assert.strictEqual(event.customer.name, 'Employee Absence');
+        });
+    });
+
+
+    describe('Matching suggestions', () => {
+        it('{4SUBSEA FLEXSHARZ} should suggest {4SUBSEA FLEXSHARE}', () => {
+            testEvent.body = 'Hello this is an event [4SUBSEA FLEXSHARZ]';
+            const event = _.first(matchEvents([testEvent], projects, customers));
+            assert.strictEqual(event.suggestedProject.key, '4SUBSEA FLEXSHARE');
+        });
+
+        it('{4SUBSEA FLEXSHARZE} should suggest {4SUBSEA FLEXSHARE}', () => {
+            testEvent.body = 'Hello this is an event [4SUBSEA FLEXSHARZE]';
+            const event = _.first(matchEvents([testEvent], projects, customers));
+            assert.strictEqual(event.suggestedProject.key, '4SUBSEA FLEXSHARE');
+        });
+
+        it('{IAM VAK} should suggest {IAM VAC}', () => {
+            testEvent.categories.push('IAM VAK');
+            const event = _.first(matchEvents([testEvent], projects, customers));
+            assert.strictEqual(event.suggestedProject.key, 'IAM VAC');
+        });
+
+        it('{IAM WHAAT} in category should yield no project but a match against Employee Absence', () => {
+            testEvent.categories.push('{IAM WHAAT}');
+            const event = _.first(matchEvents([testEvent], projects, customers));
+            assert.strictEqual(event.customer.name, 'Employee Absence');
+        });
+
+        it('4SUBSEA ABC in category should yield no project but a match against 4SUBSEA', () => {
+            testEvent.categories.push('4SUBSEA ABC');
+            const event = _.first(matchEvents([testEvent], projects, customers));
+            assert.strictEqual(event.customer.key, '4SUBSEA');
+        });
+
+        it('4SUBSEA ABC in body should yield no project and no match against customer 4SUBSEA', () => {
+            testEvent.body = 'Hello this is an event 4SUBSEA ABC';
+            const event = _.first(matchEvents([testEvent], projects, customers));
+            assert.strictEqual(event.customer, undefined);
+        });
+    });
+});
