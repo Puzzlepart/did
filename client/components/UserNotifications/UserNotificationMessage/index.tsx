2e772589aaae18d188ae8b7985a56b32b0f483d1
olemp <olemp@puzzlepart.com>
2020-05-26 11:27:12 +0200

    Resource for moreLinkText

--- client/components/UserNotifications/UserNotificationMessage/index.tsx
+++ client/components/UserNotifications/UserNotificationMessage/index.tsx
@@ -1,19 +1,24 @@
 import { MessageBar } from 'office-ui-fabric-react/lib/MessageBar'
+import { Link } from 'office-ui-fabric-react/lib/Link'
 import * as React from 'react'
 import { IUserNotificationMessageProps } from './types'
 import styles from './UserNotificationMessage.module.scss'
+import { useTranslation } from 'react-i18next'
 
 /**
  * @category UserNotifications
  */
 export const UserNotificationMessage = ({ model, onDismiss }: IUserNotificationMessageProps) => {
+    const { t } = useTranslation('common')
     return (
         <MessageBar
             {...model.messageBarProps}
             onDismiss={() => onDismiss(model)}
             className={styles.root}>
             <span>{model.text} </span>
-            {model.moreLink && <a href={model.moreLink}>Read more here</a>}
+            {model.moreLink && (
+                <Link target='_blank' href={model.moreLink}>{t('moreLinkText')}</Link>
+            )}
         </MessageBar>
     )
 }
