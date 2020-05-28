bd9150d3e8e603a1acc16e991df368d758371a43
olemp <olemp@puzzlepart.com>
2020-05-28 19:43:08 +0200

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -95,7 +95,7 @@ selectNodeVersion
 if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd "$DEPLOYMENT_TARGET"
   echo "Running $NPM_CMD install --production"
-  eval $NPM_CMD install --production
+  eval $NPM_CMD install --production --loglevel=error --no-progress --no-shrinkwrap --no-fund   
   exitWithMessageOnError "Installation of npm packages failed"
   cd - > /dev/null
 fi
