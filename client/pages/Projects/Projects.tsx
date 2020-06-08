2083bfd7f4ffe55f59a4c1b51556b4e8ad19c21d
olemp <olemp@puzzlepart.com>
2020-06-08 12:58:46 +0200

    Deep linking for Projects

--- client/pages/Projects/Projects.tsx
+++ client/pages/Projects/Projects.tsx
@@ -1,5 +1,7 @@
 import { useQuery } from '@apollo/react-hooks'
+import { AppContext } from 'AppContext'
 import { UserMessage } from 'components/UserMessage'
+import { manageProjects } from 'config/security/permissions'
 import { value } from 'helpers'
 import { IOutlookCategory, IProject } from 'interfaces'
 import { SelectionMode } from 'office-ui-fabric-react/lib/DetailsList'
@@ -9,26 +11,27 @@ import { CreateProjectForm } from 'pages/Projects/CreateProjectForm'
 import * as React from 'react'
 import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { useParams } from 'react-router-dom'
-import { find, contains } from 'underscore'
+import { useHistory, useParams } from 'react-router-dom'
+import { contains, find } from 'underscore'
 import { ProjectDetails } from './ProjectDetails'
 import ProjectList from './ProjectList'
-import { GET_PROJECTS, IGetProjectsData } from './types'
-import { AppContext } from 'AppContext'
-import { manageProjects } from 'config/security/permissions'
+import { GET_PROJECTS, IGetProjectsData, IProjectsParams } from './types'
 
 /**
  * @category Projects
  */
 export const Projects = () => {
     const { t } = useTranslation(['projects', 'common'])
+    const history = useHistory()
     const { user } = React.useContext(AppContext)
-    const params = useParams<{ key: string }>()
+    const params = useParams<IProjectsParams>()
     const [selected, setSelected] = useState<IProject>(null)
     const { loading, error, data } = useQuery<IGetProjectsData>(GET_PROJECTS, { variables: { sortBy: 'name' }, fetchPolicy: 'cache-first' })
 
     const outlookCategories = value<IOutlookCategory[]>(data, 'outlookCategories', [])
-    const projects = value<IProject[]>(data, 'projects', []).map(p => ({ ...p, outlookCategory: find(outlookCategories, c => c.displayName === p.key) }))
+    const projects = value<IProject[]>(data, 'projects', []).map(p => ({
+        ...p, outlookCategory: find(outlookCategories, c => c.displayName === p.key),
+    }))
 
     React.useEffect(() => {
         if (!selected && params.key) {
@@ -37,8 +40,15 @@ export const Projects = () => {
         }
     }, [params.key, projects])
 
+    function onChangeTab({ props }: PivotItem) {
+        history.push(`/projects/${props.itemKey}`)
+    }
+
     return (
-        <Pivot styles={{ itemContainer: { paddingTop: 10 } }}>
+        <Pivot
+            defaultSelectedKey={params.view}
+            onLinkClick={onChangeTab}
+            styles={{ itemContainer: { paddingTop: 10 } }}>
             <PivotItem
                 itemID='search'
                 itemKey='search'
@@ -62,8 +72,8 @@ export const Projects = () => {
                     )}
             </PivotItem>
             <PivotItem
-                itemID='myprojects'
-                itemKey='myprojects'
+                itemID='my'
+                itemKey='my'
                 headerText={t('myProjectsText')}
                 itemIcon='FabricUserFolder'>
                 {error
