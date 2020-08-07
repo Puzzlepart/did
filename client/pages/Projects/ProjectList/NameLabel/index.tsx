9834b677e1494b4c0576aded88f4b305e56620d9
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-08-07 13:08:58 +0200

    Added component NameLabel

---
+++ client/pages/Projects/ProjectList/NameLabel/index.tsx
@@ -0,0 +1,26 @@
+import { Icon } from 'office-ui-fabric-react/lib/Icon'
+import React, { useState, useRef } from 'react'
+import { Link } from 'react-router-dom'
+import { ContextualMenu } from 'office-ui-fabric-react'
+import styles from './NameLabel.module.scss'
+
+export const NameLabel = ({ project, actions }) => {
+    const target = useRef()
+    const [showMenu, setShowMenu] = useState(false)
+    return (
+        <div className={styles.root}>
+            <Link to={`/projects/${project.id}`}>{project.name}</Link>
+            <span
+                ref={target}
+                onClick={() => setShowMenu(true)}
+                className={styles.menuToggle} >
+                <Icon iconName='More' />
+            </span>
+            <ContextualMenu
+                hidden={!showMenu}
+                onDismiss={() => setShowMenu(false)}
+                target={target.current}
+                items={actions} />
+        </div>
+    )
+}
