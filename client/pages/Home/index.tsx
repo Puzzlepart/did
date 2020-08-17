c75b2e8b4e962459aeb0821e7d7b015d94ba1256
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-08-17 13:44:47 +0200

    Example charts added to Home #265

--- client/pages/Home/index.tsx
+++ client/pages/Home/index.tsx
@@ -1,5 +1,39 @@
 import * as React from 'react'
+import { PieChart, Pie, Tooltip, Legend } from 'recharts'
 import styles from './Home.module.scss'
+
+const data01 = [
+    {
+        'name': 'Used',
+        'value': 160
+    },
+    {
+        'name': 'Remaining',
+        'value': 40
+    },
+]
+
+const data02 = [
+    {
+        'name': 'Confirmed',
+        'value': 25
+    },
+    {
+        'name': 'Unconfirmed',
+        'value': 2
+    },
+]
+
+const data03 = [
+    {
+        'name': 'DSS',
+        'value': 32
+    },
+    {
+        'name': 'Hexagon Ragasco',
+        'value': 8
+    },
+]
 
 /**
  * @ignore
@@ -9,6 +43,30 @@ export default () => {
         <div className={styles.root}>
             <img src='/images/did365logobeta.png' className={styles.logo} />
             <p className={styles.motto}>The Calendar is the Timesheet</p>
+            <div className={styles.charts}>
+                <div>
+                    <h4>Vacation</h4>
+                    <PieChart width={300} height={250}>
+                        <Pie data={data01} dataKey='value' nameKey='name' cx='50%' cy='50%' outerRadius={50} fill='#8884d8' label />
+                        <Tooltip />
+                        <Legend />
+                    </PieChart>
+                </div>
+                <div>
+                    <h4>Confirmed weeks</h4>
+                    <PieChart width={300} height={250}>
+                        <Pie data={data02} dataKey='value' nameKey='name' cx='50%' cy='50%' outerRadius={50} fill='#8884d8' label />
+                    </PieChart>
+                </div>
+                <div>
+                    <h4>Allocation this week</h4>
+                    <PieChart width={300} height={250}>
+                        <Pie data={data03} dataKey='value' nameKey='name' cx='50%' cy='50%' outerRadius={50} fill='#8884d8' label />
+                        <Tooltip />
+                        <Legend />
+                    </PieChart>
+                </div>
+            </div>
         </div>
     )
 }
