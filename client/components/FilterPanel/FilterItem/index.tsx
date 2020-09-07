359438796116572e1ef87f15631c4d34fac80e86
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-09-07 15:54:26 +0200

    Minor syntax adjustments on FilterItem

--- client/components/FilterPanel/FilterItem/index.tsx
+++ client/components/FilterPanel/FilterItem/index.tsx
@@ -1,7 +1,7 @@
 import { Checkbox } from 'office-ui-fabric-react/lib/Checkbox'
 import React, { useState, useMemo } from 'react'
 import styles from './FilterItem.module.scss'
-import { IFilterItemProps } from './IFilterItemProps'
+import { IFilterItemProps } from './types'
 import { SearchBox } from 'office-ui-fabric-react/lib/SearchBox'
 import { contains, isBlank } from 'underscore.string'
 import { useTranslation } from 'react-i18next'
@@ -9,19 +9,19 @@ import { useTranslation } from 'react-i18next'
 /**
  * @category FilterPanel
  */
-export const FilterItem = ({ filter, onFilterUpdated }: IFilterItemProps) => {
+export const FilterItem = (props: IFilterItemProps) => {
     const { t } = useTranslation('common')
-    const selectedKeys = filter.selected.map(f => f.key)
+    const selectedKeys = props.filter.selected.map(f => f.key)
     const [searchTerm, onSearch] = useState<string>('')
 
     const items = useMemo(() => {
-        return filter.items.filter(item => isBlank(searchTerm) ? true : contains(item.value.toLowerCase(), searchTerm.toLowerCase()))
+        return props.filter.items.filter(item => isBlank(searchTerm) ? true : contains(item.value.toLowerCase(), searchTerm.toLowerCase()))
     }, [searchTerm])
 
     return (
-        <div key={filter.key} className={styles.root}>
-            <div className={styles.name}>{filter.name}</div>
-            <div className={styles.searchBox} hidden={filter.items.length < 10}>
+        <div className={styles.root}>
+            <div className={styles.name}>{props.filter.name}</div>
+            <div className={styles.searchBox} hidden={props.filter.items.length < 10}>
                 <SearchBox
                     placeholder={t('searchPlaceholder')}
                     onChange={(_event, value) => onSearch(value)} />
@@ -31,7 +31,7 @@ export const FilterItem = ({ filter, onFilterUpdated }: IFilterItemProps) => {
                     <Checkbox
                         label={item.value}
                         checked={selectedKeys.indexOf(item.key) !== -1}
-                        onChange={(_, checked) => onFilterUpdated(filter, item, checked)} />
+                        onChange={(_, checked) => props.onFilterUpdated(props.filter, item, checked)} />
                 </div>
             ))}
         </div >
