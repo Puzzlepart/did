fcbc9e77c1e035fa9cb90c1df82d52fd190487b2
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-09-24 19:37:43 +0200

    i18n syntax

--- client/i18n.ts
+++ client/i18n.ts
@@ -1,24 +1,15 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import resources from '../resources'
 
 i18n.use(initReactI18next).init({
-  resources: require('../resources').default,
+  resources,
   fallbackLng: 'nb',
   debug: process.env.NODE_ENV === 'development',
   interpolation: {
     escapeValue: false,
   },
-  ns: [
-    'timesheet',
-    'projects',
-    'customers',
-    'admin',
-    'reports',
-    'common',
-    'navigation',
-    'permissions',
-    'notifications',
-  ],
+  ns: Object.keys(resources['en-GB']),
   nsSeparator: '.',
   keySeparator: ':',
 })
