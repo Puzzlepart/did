175198aea163365b81af818b715cf6e5617e5aed
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-09-30 22:28:02 +0200

    Updated sample.env

--- tests/ensureTestData.js
+++ tests/ensureTestData.js
@@ -9,31 +9,33 @@ let storage = new StorageService({
 })
 
 module.exports = () => new Promise((resolve, reject) => {
-  if(!process.env.TESTS_AZURE_STORAGE_CONNECTION_STRING) {
-    reject('Missing environment variable TESTS_AZURE_STORAGE_CONNECTION_STRING')
-  }
-  try {
-    const data = require('./testData.json')
-    log('testData.json found')
-    resolve(data)
-  } catch (error) {
-    log('testData.json missing. Querying Azure Table Storage...')
-    Promise.all([
-      storage.getProjects(),
-      storage.getCustomers(),
-      storage.getLabels()
-    ]).then(value => {
-      fs.writeFile(
-        path.resolve(__dirname, 'testData.json'),
-        JSON.stringify(value),
-        null,
-        () => {
-          log('Saving result to testData.json')
-          resolve(value)
-        })
-    }).catch(error => {
-      log('An error occured querying Azure Table Storage...')
-      reject(error)
-    })
+  if (!process.env.TESTS_AZURE_STORAGE_CONNECTION_STRING) {
+    log('Missing environment variable TESTS_AZURE_STORAGE_CONNECTION_STRING')
+    reject()
+  } else {
+    try {
+      const data = require('./testData.json')
+      log('testData.json found')
+      resolve(data)
+    } catch (error) {
+      log('testData.json missing. Querying Azure Table Storage...')
+      Promise.all([
+        storage.getProjects(),
+        storage.getCustomers(),
+        storage.getLabels()
+      ]).then(value => {
+        fs.writeFile(
+          path.resolve(__dirname, 'testData.json'),
+          JSON.stringify(value),
+          null,
+          () => {
+            log('Saving result to testData.json')
+            resolve(value)
+          })
+      }).catch(error => {
+        log('An error occured querying Azure Table Storage...')
+        reject(error)
+      })
+    }
   }
 })
