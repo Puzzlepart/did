c623f53640ab56cc38e3eaa4f1ea0bb416033b1b
Kim Damsleth <kim@damsleth.com>
2020-10-07 10:19:52 +0200

    MobileHeader <span> -> <div>

--- client/components/MobileHeader/index.tsx
+++ client/components/MobileHeader/index.tsx
@@ -5,11 +5,11 @@ import { IMobileHeaderProps } from './types'
 
 export const MobileHeader = (props: IMobileHeaderProps) => {
     return (
-        <span className={styles.root}>
+        <div className={styles.root}>
             <Icon iconName={props.iconName} className={styles.headerIcon} />
             <span className={styles.headerText}>
                 {props.text}
             </span>
-        </span>
+        </div>
     )
 }
