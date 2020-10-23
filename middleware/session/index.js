d1630ea42649261d1e5e4fc34db616634721ed61
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-10-23 17:22:13 +0200

    Setting SESSION_TIMEOUT in process.env

--- middleware/session/index.js
+++ middleware/session/index.js
@@ -2,14 +2,12 @@ const session = require('express-session')
 const connectAzureTables = require('connect-azuretables')(session)
 const env = require('../../utils/env')
 
-const store = connectAzureTables.create({
-  table: 'Sessions',
-  sessionTimeOut: 10080,
-})
-
 module.exports = session({
   name: env('SESSION_NAME'),
-  store: store,
+  store: connectAzureTables.create({
+    table: 'Sessions',
+    sessionTimeOut: parseInt(env('SESSION_TIMEOUT', '10080'))
+  }),
   secret: env('SESSION_SIGNING_KEY'),
   resave: false,
   saveUninitialized: false,
