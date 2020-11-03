a961d00a1eec3f66f9fbc52e734bb02f3d664af5
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-11-03 17:31:58 +0100

    Misc

--- server/services/msgraph.event.ts
+++ server/services/msgraph.event.ts
@@ -12,15 +12,15 @@ export default class MSGraphEvent {
   public duration: any
 
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  constructor(evt: any) {
-    this.id = evt.id
-    this.title = evt.subject
-    this.body = utils.stripHtmlString(evt.body.content)
-    this.isOrganizer = evt.isOrganizer
-    this.categories = evt.categories
-    this.webLink = evt.webLink
-    this.startDateTime = evt.start.dateTime
-    this.endDateTime = evt.end.dateTime
-    this.duration = utils.getDurationHours(evt.start.dateTime, evt.end.dateTime)
+  constructor(event: any) {
+    this.id = event.id
+    this.title = event.subject
+    this.body = utils.stripHtmlString(event.body.content)
+    this.isOrganizer = event.isOrganizer
+    this.categories = event.categories
+    this.webLink = event.webLink
+    this.startDateTime = event.start.dateTime
+    this.endDateTime = event.end.dateTime
+    this.duration = utils.getDurationHours(event.start.dateTime, event.end.dateTime)
   }
 }
