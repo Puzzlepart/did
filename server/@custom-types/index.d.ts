aa317a7729faff82e962158667b71f5b95bb77fa
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-11-04 23:24:16 +0100

    @custom-types

--- server/@custom-types/index.d.ts
+++ server/@custom-types/index.d.ts
@@ -1,36 +1,26 @@
-export interface Subscription {
-    partitionKey: string;
-    id: string;
-    timestamp: Date;
-    connectionString: string;
-    name: string;
-    settings: any;
-}
-
-export interface TokenParams {
-    token_type: string;
-    scope: string;
-    expires_in: number;
-    ext_expires_in: number;
-    access_token: string;
-    refresh_token: string;
-    id_token: string;
-}
-
 declare namespace Express {
-    interface User {
+    interface UserSubscription {
         partitionKey: string;
         id: string;
         timestamp: Date;
-        displayName: string;
-        givenName: string;
-        jobTitle: string;
-        mail: string;
-        mobilePhone: string;
-        preferredLanguage: string;
-        role: string;
-        surname: string;
-        subscription: Subscription;
-        tokenParams: TokenParams;
+        connectionString: string;
+        name: string;
+        settings: any;
+    }
+
+    interface User {
+        partitionKey?: string;
+        id?: string;
+        timestamp?: Date;
+        displayName?: string;
+        givenName?: string;
+        jobTitle?: string;
+        mail?: string;
+        mobilePhone?: string;
+        preferredLanguage?: string;
+        role?: string;
+        surname?: string;
+        subscription?: UserSubscription;
+        tokenParams?: any;
     }
 }
