b406b878f9ad62c66be7af651dca0e4c2dd1a344
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-11-07 16:12:10 +0100

    Removed all gql imports

--- client/pages/Admin/Subscription/GET_SUBSCRIPTION.ts
+++ client/pages/Admin/Subscription/GET_SUBSCRIPTION.ts
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client'
+
 
 export const GET_SUBSCRIPTION = gql`
   {
--- client/pages/Admin/Users/BULK_ADD_USERS.ts
+++ client/pages/Admin/Users/BULK_ADD_USERS.ts
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client'
+
 
 export default gql`
   mutation($users: [UserInput]!) {
