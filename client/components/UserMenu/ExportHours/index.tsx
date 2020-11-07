e58208c5b68725db591f1ca58855c2ea9bf94f72
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-11-07 15:58:46 +0100

    timeentriesQuery  from './timeentries.gql'

--- client/components/UserMenu/ExportHours/index.tsx
+++ client/components/UserMenu/ExportHours/index.tsx
@@ -11,7 +11,7 @@ import { exportExcel } from 'utils/exportExcel'
 import { client } from '../../../graphql'
 import columns from '../columns'
 import styles from '../UserMenu.module.scss'
-import { query } from './graphql'
+import timeentriesQuery  from './timeentries.gql'
 import { getExportTypes, IExportType } from './types'
 
 export const ExportHours: React.FunctionComponent = () => {
@@ -36,7 +36,7 @@ export const ExportHours: React.FunctionComponent = () => {
 
     const onExport = async () => {
         const { data } = await client.query({
-            query: query.timeentries,
+            query: timeentriesQuery,
             variables: exportType.variables,
         })
         await exportExcel(
