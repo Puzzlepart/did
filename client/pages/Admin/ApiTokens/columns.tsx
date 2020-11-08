847a38729bf8f7b860348839682ad61da30cbd42
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-11-08 20:24:46 +0100

    Updated ApiTokens component

---
+++ client/pages/Admin/ApiTokens/columns.tsx
@@ -0,0 +1,48 @@
+import { TFunction } from 'i18next'
+import { DefaultButton } from 'office-ui-fabric-react/lib/Button'
+import { IColumn } from 'office-ui-fabric-react/lib/DetailsList'
+import React from 'react'
+import { ApiToken } from 'types'
+import dateUtils from 'utils/date'
+
+/**
+ * Returns the columns for the ApiTokens list
+ * 
+ * @param {void} onEdit On edit callback
+ * @param {TFunction} t Translate function
+ */
+export const ApiTokensColumns = (onDeleteApiToken: (token: ApiToken) => void, t: TFunction): IColumn[] => ([
+    {
+        key: 'name',
+        fieldName: 'name',
+        name: t('common.nameFieldLabel'),
+        minWidth: 100,
+        maxWidth: 250,
+    },
+    {
+        key: 'created',
+        name: t('common.createdLabel'),
+        minWidth: 100,
+        maxWidth: 180,
+        onRender: (token: ApiToken) => dateUtils.formatDate(token.created, 'LLL')
+    },
+    {
+        key: 'expires',
+        name: t('common.expiresLabel'),
+        minWidth: 100,
+        onRender: (token: ApiToken) => dateUtils.formatDate(token.expires, 'LLL')
+    },
+    {
+        key: 'actions',
+        name: '',
+        minWidth: 150,
+        onRender: (token: ApiToken) => {
+            return (
+                <DefaultButton
+                    text={t('common.delete')}
+                    iconProps={{ iconName: 'RecycleBin' }}
+                    onClick={() => onDeleteApiToken(token)} />
+            )
+        }
+    }
+])
--- client/pages/Admin/ApiTokens/index.tsx
+++ client/pages/Admin/ApiTokens/index.tsx
@@ -1,125 +1,45 @@
 import { useMutation, useQuery } from '@apollo/client'
-import { UserMessage } from 'components'
+import { useMessage, UserMessage } from 'components'
 import List from 'components/List'
-import { DefaultButton } from 'office-ui-fabric-react/lib/Button'
 import { MessageBarType } from 'office-ui-fabric-react/lib/MessageBar'
-import { TextField } from 'office-ui-fabric-react/lib/TextField'
-import React, { useState } from 'react'
+import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { isBlank } from 'underscore.string'
-import { sleep } from 'utils'
-import dateUtils from 'utils/date'
-import $addApiToken from './addApiToken.gql'
+import { ApiToken } from 'types'
+import { ApiTokenForm } from './ApiTokenForm'
 import styles from './ApiTokens.module.scss'
+import { ApiTokensColumns as columns } from './columns'
 import $deleteApiToken from './deleteApiToken.gql'
 import $tokens from './tokens.gql'
 
 export const ApiTokens = () => {
     const { t } = useTranslation()
-    const [key, setKey] = useState(null)
-    const [name, setName] = useState(null)
-    const [message, setMessage] = useState(null)
-    const [addApiToken, { loading }] = useMutation($addApiToken)
+    const [message, setMessage] = useMessage()
     const [deleteApiToken] = useMutation($deleteApiToken)
     const { data, refetch } = useQuery($tokens)
 
     /**
-     * On add API token
-     */
-    async function onAddApiToken() {
-        const { data } = await addApiToken({ variables: { name } })
-        if (data.key) {
-            setKey(data.key)
-            setMessage({
-                type: MessageBarType.success,
-                children: t('admin.tokenGeneratedText'),
-            })
-            setName(null)
-            refetch()
-            await sleep(10)
-        } else {
-            setMessage({
-                type: MessageBarType.error,
-                text: t('admin.tokenErrorText'),
-            })
-            setName(null)
-            await sleep(5)
-        }
-        setKey(null)
-        setMessage(null)
-    }
-
-    /**
      * On delete API token
      * 
-     * @param token The token to dete 
+     * @param {ApiToken} token The token to dete 
      */
-    async function onDeleteApiToken(token) {
-        await deleteApiToken({ variables: { name: token.name } })
+    async function onDeleteApiToken(token: ApiToken) {
+        // await deleteApiToken({ variables: { token } })
         setMessage({
             type: MessageBarType.info,
-            text: t('admin.tokenDeletedText', { name: token.name }),
+            text: t('admin.tokenDeletedText', token),
         })
-        setName(null)
-        setKey(null)
-        refetch()
+        // setName(null)
+        // setKey(null)
+        // refetch()
     }
-    
+
 
     return (
         <div className={styles.root}>
-            <div className={styles.form}>
-                <TextField
-                    placeholder={t('admin.tokenNamePlaceholder')}
-                    value={name}
-                    onChange={(_event, value) => setName(value)} />
-                <DefaultButton
-                    text={t('admin.generateTokenLabel')}
-                    onClick={onAddApiToken}
-                    disabled={loading || !!key || isBlank(name)} />
-            </div>
-            {message && (
-                <UserMessage
-                    className={styles.message}
-                    {...message}
-                    iconName='AzureAPIManagement' />
-            )}
-            <TextField
-                className={styles.field}
-                value={key}
-                multiline={true}
-                disabled={true} />
+            <ApiTokenForm setMessage={setMessage} />
+            {message && <UserMessage {...message} />}
             <List
-                columns={[
-                    {
-                        key: 'name',
-                        fieldName: 'name',
-                        name: t('common.nameFieldLabel'),
-                        minWidth: 100,
-                        maxWidth: 250,
-                    },
-                    {
-                        key: 'created',
-                        fieldName: 'created',
-                        name: t('common.createdLabel'),
-                        minWidth: 100,
-                        onRender: (item) => dateUtils.formatDate(item.created, 'LLL')
-                    },
-                    {
-                        key: 'actions',
-                        fieldName: null,
-                        name: '',
-                        minWidth: 150,
-                        onRender: (item) => {
-                            return (
-                                <DefaultButton
-                                    text={t('common.delete')}
-                                    iconProps={{ iconName: 'RecycleBin' }}
-                                    onClick={() => onDeleteApiToken(item)} />
-                            )
-                        }
-                    }
-                ]}
+                columns={columns(onDeleteApiToken, t)}
                 items={data?.tokens || []} />
         </div>
     )
