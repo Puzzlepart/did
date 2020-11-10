4e60902fef7c38bd92a1536b3de654c2339fcda4
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-11-10 10:44:09 +0100

    Minor stuffiroonies

--- client/pages/Timesheet/ActionBar/WeekPicker/weekPickerCommand.tsx
+++ client/pages/Timesheet/ActionBar/WeekPicker/weekPickerCommand.tsx
@@ -1,16 +1,21 @@
 import { IContextualMenuItem } from 'office-ui-fabric'
 import * as React from 'react'
+import FadeIn from 'react-fade-in'
 import { WeekPicker } from '.'
 import { ITimesheetContext } from '../../context'
 import styles from './WeekPicker.module.scss'
 
-export default ({ selectedPeriod, periods, t }: ITimesheetContext): IContextualMenuItem => ({
+export default ({ loading,selectedPeriod, periods, t }: ITimesheetContext): IContextualMenuItem => ({
   key: 'WEEK_PICKER_COMMAND',
   onRender: () => {
     return (
       <>
         <WeekPicker />
-        {periods.length === 1 && <div className={styles.weekNumber}>{selectedPeriod.getName(t)}</div>}
+        {(periods.length === 1 && !loading) && (
+          <FadeIn className={styles.weekNumber}>
+            {selectedPeriod.getName(t)}
+          </FadeIn>
+        )}
       </>
     )
   }
