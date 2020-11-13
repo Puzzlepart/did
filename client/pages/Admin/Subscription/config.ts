61a3500a01f011c46793de9475daf2723f00d095
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-11-13 19:26:14 +0100

    Updated config

--- client/pages/Admin/Subscription/config.ts
+++ client/pages/Admin/Subscription/config.ts
@@ -1,5 +1,5 @@
 import { TFunction } from 'i18next'
-import { SubscriptionForecastSettingsInput } from 'types'
+import { SubscriptionADSyncSettingsInput, SubscriptionForecastSettingsInput } from 'types'
 import { ISettingsSectionProps } from './SettingsSection/types'
 
 /**
@@ -8,6 +8,40 @@ import { ISettingsSectionProps } from './SettingsSection/types'
  * @param {TFunction} t Translate function
  */
 export const SUBSCRIPTION_SETTINGS = (t: TFunction): ISettingsSectionProps[] => [
+  {
+    id: 'adsync',
+    name: t('admin.adsync'),
+    fields: [
+      {
+        id: 'adUserSyncEnabled',
+        type: 'bool',
+        props: new Map<string, any>([
+          ['label', t('admin.adUserSyncEnabledLabel')],
+          ['description', t('admin.adUserSyncEnabledDescription')]
+        ])
+      },
+      {
+        id: 'adUserSyncProperties',
+        type: 'checkbox',
+        options: {
+          givenName: 'Given name',
+          surname: 'Surname',
+          jobTitle: 'Job title',
+          displayName: 'Display name',
+          mobilePhone: 'Mobile phone',
+          mail: 'Mail',
+          preferredLanguage: 'PreferredLanguage',
+          department: 'Department',
+          officeLocation: 'Office location',
+        },
+        props: new Map<string, any>([
+          ['label', t('admin.adUserSyncPropertiesLabel')],
+          ['description', t('admin.adUserSyncPropertiesDescription')]
+        ]),
+        hiddenIf: (settings: SubscriptionADSyncSettingsInput) => !settings.adUserSyncEnabled
+      }
+    ]
+  },
   {
     id: 'forecast',
     name: t('admin.forecasting'),
@@ -36,3 +70,4 @@ export const SUBSCRIPTION_SETTINGS = (t: TFunction): ISettingsSectionProps[] => 
     ]
   }
 ]
+
