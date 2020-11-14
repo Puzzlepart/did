e5ebcc4d69dc26dd30954a60c0eee7cf52ee43d8
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-11-14 15:46:44 +0100

    Home: Rendering sign in error

--- client/pages/Home/index.tsx
+++ client/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import { AppContext } from 'AppContext'
-import { DefaultButton } from 'office-ui-fabric'
+import { UserMessage } from 'components'
+import { DefaultButton, MessageBarType } from 'office-ui-fabric'
 import React, { useContext } from 'react'
 import { useTranslation } from 'react-i18next'
 import styles from './Home.module.scss'
@@ -7,11 +8,23 @@ import styles from './Home.module.scss'
 export default (): React.ReactElement<HTMLDivElement> => {
   const { subscription } = useContext(AppContext)
   const { t } = useTranslation()
+  const error = JSON.parse(document.getElementById('app').getAttribute('data-error') || null)
+
   return (
     <div className={styles.root}>
       <div className={styles.logo}>did</div>
       <p className={styles.motto}>{t('common.motto')}</p>
-      <div hidden={!!subscription}>
+      {error && (
+        <UserMessage
+          className={styles.error}
+          type={MessageBarType.error}
+          iconName={error.icon}
+          text={[
+            `#### ${error.name} ####`,
+            error.message
+          ].join('\n\n')} />
+      )}
+      <div hidden={!!subscription || !!error}>
         <DefaultButton className={styles.signinbutton} href='/auth/signin' text={t('common.signInText')} />
       </div>
     </div>
