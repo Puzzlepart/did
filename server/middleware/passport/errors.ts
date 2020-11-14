2510b2984a1a0c80b35b23de7d4d6030fc9440d8
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-11-14 15:50:39 +0100

    Sign in errors

--- server/middleware/passport/errors.ts
+++ server/middleware/passport/errors.ts
@@ -1,13 +1,50 @@
-export const NO_OID_FOUND = new Error()
-NO_OID_FOUND.name = 'Sorry to break it to you..'
-NO_OID_FOUND.message = '... but an error occured attempting to sign you in.'
+export class SigninError extends Error {
+  constructor(public name: string, message: string, public icon?: string) {
+    super(message)
+  }
 
-export const TENANT_NOT_ENROLLED = new Error()
-TENANT_NOT_ENROLLED.name = 'I hate to be the one telling you this...'
-TENANT_NOT_ENROLLED.message =
-  '... but your company is not enrolled in did. Please contact <a href="mailto:did@puzzlepart.com">did@puzzlepart.com</a> for more information.'
+  /**
+   * Returns a string representation of the SigninError
+   */
+  toString() {
+    return JSON.stringify({
+      name: this.name,
+      message: this.message,
+      icon: this.icon
+    })
+  }
+}
 
-export const USER_NOT_ENROLLED = new Error()
-USER_NOT_ENROLLED.name = 'I promised to keep it a secret...'
-// eslint-disable-next-line quotes
-USER_NOT_ENROLLED.message = "...but it seems you're not enrolled in did. Please contact your system owner."
+/**
+ * Error for no user ID found
+ * 
+ * Markdown is supported
+ */
+export const NO_OID_FOUND = new SigninError(
+  'Sorry to break it to you..',
+  '... but an error occured attempting to sign you in.',
+  'BlockedSite'
+)
+
+/**
+ * Error for no subscription found for the tenant ID
+ * 
+ * Markdown is supported
+ */
+export const TENANT_NOT_ENROLLED = new SigninError(
+  'I hate to be the one telling you this...',
+  '... but your company is not enrolled in did.<br/><br/> Please contact <a href="mailto:did@puzzlepart.com">did@puzzlepart.com</a> for more information.',
+  'Phone'
+)
+
+/**
+ * Error for user not found in subscription directory
+ * 
+ * Markdown is supported
+ */
+export const USER_NOT_ENROLLED = new SigninError(
+  'I promised to keep it a secret...',
+  // eslint-disable-next-line quotes
+  "...but it seems you're not enrolled in did.<br/><br/> Please contact your system owner.",
+  'Sad'
+)
