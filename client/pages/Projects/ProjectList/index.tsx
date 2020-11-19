6ea8fbeec29fccbf797c7ba36e082373d334d549
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-11-19 16:40:14 +0100

    Re-added key to Projects query

--- client/pages/Projects/ProjectList/index.tsx
+++ client/pages/Projects/ProjectList/index.tsx
@@ -45,4 +45,10 @@ const ProjectList: FunctionComponent<IProjectListProps> = (props: IProjectListPr
   )
 }
 
-export default withDefaultProps(ProjectList, { items: [], hideColumns: [] })
+export default withDefaultProps(
+  ProjectList,
+  {
+    items: [],
+    hideColumns: []
+  }
+)
