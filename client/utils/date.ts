18eb9f266547f0343f59ff706aa9f4027e96ea54
Kim Damsleth <kim@damsleth.com>
2020-12-16 12:00:36 +0100

    getIsoWeek implementation for 53-week split years

--- client/utils/date.ts
+++ client/utils/date.ts
@@ -7,230 +7,241 @@ import localeDataPlugin from 'dayjs/plugin/localeData'
 import objectSupportPlugin from 'dayjs/plugin/objectSupport'
 import utcPlugin from 'dayjs/plugin/utc'
 import weekOfYearPlugin from 'dayjs/plugin/weekOfYear'
-import isoWeeksInYear  from 'dayjs/plugin/isoWeeksInYear'
-import isLeapYear  from 'dayjs/plugin/isLeapYear'
+import isoWeeksInYear from 'dayjs/plugin/isoWeeksInYear'
+import isLeapYear from 'dayjs/plugin/isLeapYear'
 import { TFunction } from 'i18next'
 import { capitalize } from 'underscore.string'
 import { DateObject } from './date.dateObject'
 
 interface IDateUtils {
-  /**
-   * Timezone offset
-   *
-   * Retrieved from Date.getTimezoneOffset()
-   */
-  tzOffset: number
+    /**
+     * Timezone offset
+     *
+     * Retrieved from Date.getTimezoneOffset()
+     */
+    tzOffset: number
 
-  /**
-   * Default month format
-   */
-  monthFormat: string
+    /**
+     * Default month format
+     */
+    monthFormat: string
 
-  /**
-   * Use ISO week
-   */
-  isoWeek: boolean
+    /**
+     * Use ISO week
+     */
+    isoWeek: boolean
 }
 
 export type DateInput = ConfigType
 
 export class DateUtils {
-  constructor(private $: IDateUtils) { }
+    constructor(private $: IDateUtils) { }
 
-  /**
-   * Setup DateUtils class using @dayjs with @plugins
-   *
-   * @param {string} locale Locale
-   */
-  public setup(locale: string) {
-    $dayjs.locale(locale)
-    $dayjs.extend<PluginFunc>(weekOfYearPlugin)
-    $dayjs.extend<PluginFunc>(localeDataPlugin)
-    $dayjs.extend<PluginFunc>(durationPlugin)
-    $dayjs.extend<PluginFunc>(objectSupportPlugin)
-    $dayjs.extend<PluginFunc>(utcPlugin)
-    $dayjs.extend<PluginFunc>(isoWeekPlugin)
-    $dayjs.extend<PluginFunc>(isoWeeksInYear)
-    $dayjs.extend<PluginFunc>(isLeapYear)
-  }
-
-  /**
-   * Subtract timezone offset
-   *
-   * @param {DateInput} date Date
-   */
-  private _fixTzOffset(date: DateInput) {
-    return $dayjs(date).subtract(this.$.tzOffset, 'minute')
-  }
-
-  /**
-   * Get duration string
-   *
-   * @param {number} hours Duration in hours
-   * @param {TFunction} t Translate function
-   */
-  public getDurationString(hours: number, t: TFunction): string {
-    const hrs = Math.floor(hours)
-    const mins = parseInt(((hours % 1) * 60).toFixed(0))
-    const hrsStr = t('common.hoursShortFormat', { hrs })
-    const minsStr = t('common.minutesShortFormat', { mins })
-    if (mins === 0) return hrsStr
-    if (hrs === 0) return minsStr
-    return [hrsStr, minsStr].join(' ')
-  }
-
-  /**
-   * Format date with the specified date format
-   *
-   * @param {DateInput} date Date
-   * @param {string} template Date format
-   */
-  public formatDate(date: DateInput, template: string): string {
-    return this._fixTzOffset(date).format(template)
-  }
-
-  /**
-   * Get start of week
-   *
-   * @param {DateObject} date Date
-   * @param {boolean} isoWeek Use ISO week
-   */
-  public startOfWeek(date?: DateObject, isoWeek: boolean = this.$.isoWeek): DateObject {
-    return new DateObject(date.$.startOf(isoWeek ? 'isoWeek' : 'w'))
-  }
-
-  /**
-   * Get end of week
-   *
-   * @param {DateObject} date Date
-   * @param {boolean} isoWeek Use ISO week
-   */
-  public endOfWeek(date?: DateObject, isoWeek: boolean = this.$.isoWeek): DateObject {
-    return new DateObject(date.$.endOf(isoWeek ? 'isoWeek' : 'w'))
-  }
-
-  /**
-   * Get days between a start and end time in the specified template
-   *
-   * @param {DateInput} start Start
-   * @param {DateInput} end End
-   * @param {string} template Date template
-   */
-  getDays(start: DateInput, end: DateInput, template: string = 'dddd DD'): string[] {
-    const days = []
-    let s = new DateObject(start)
-    const e = new DateObject(end)
-    while (s.isBeforeOrSame(e)) {
-      days.push(capitalize(s.format(template)))
-      s = s.add('1d')
+    /**
+     * Setup DateUtils class using @dayjs with @plugins
+     *
+     * @param {string} locale Locale
+     */
+    public setup(locale: string) {
+        $dayjs.locale(locale)
+        $dayjs.extend<PluginFunc>(weekOfYearPlugin)
+        $dayjs.extend<PluginFunc>(localeDataPlugin)
+        $dayjs.extend<PluginFunc>(durationPlugin)
+        $dayjs.extend<PluginFunc>(objectSupportPlugin)
+        $dayjs.extend<PluginFunc>(utcPlugin)
+        $dayjs.extend<PluginFunc>(isoWeekPlugin)
+        $dayjs.extend<PluginFunc>(isoWeeksInYear)
+        $dayjs.extend<PluginFunc>(isLeapYear)
     }
-    return days
-  }
 
-  /**
-   * Get month name for the speicifed month index
-   *
-   * @param {number} monthIndex Month index
-   * @param {string} template Template
-   */
-  public getMonthName(monthIndex?: number, template: string = this.$.monthFormat): string {
-    return $dayjs().set('month', monthIndex).format(template)
-  }
+    /**
+     * Subtract timezone offset
+     *
+     * @param {DateInput} date Date
+     */
+    private _fixTzOffset(date: DateInput) {
+        return $dayjs(date).subtract(this.$.tzOffset, 'minute')
+    }
 
-  /**
-   * Get timespan string
-   *
-   * @param {DateObject} start Start
-   * @param {DateObject} end End
-   * @param {string} monthFormat Month format
-   */
-  public getTimespanString(
-    start: DateObject,
-    end: DateObject,
-    monthFormat: string = this.$.monthFormat
-  ): string {
-    const isSameMonth = start.isSameMonth(end)
-    const isSameYear = start.isSameYear(end)
-    const sFormat = ['DD']
-    if (!isSameMonth) sFormat.push(monthFormat)
-    if (!isSameYear) sFormat.push('YYYY')
-    return [start.format(sFormat.join(' ')), end.format(`DD ${monthFormat} YYYY`)].join(' - ')
-  }
+    /**
+     * Get duration string
+     *
+     * @param {number} hours Duration in hours
+     * @param {TFunction} t Translate function
+     */
+    public getDurationString(hours: number, t: TFunction): string {
+        const hrs = Math.floor(hours)
+        const mins = parseInt(((hours % 1) * 60).toFixed(0))
+        const hrsStr = t('common.hoursShortFormat', { hrs })
+        const minsStr = t('common.minutesShortFormat', { mins })
+        if (mins === 0) return hrsStr
+        if (hrs === 0) return minsStr
+        return [hrsStr, minsStr].join(' ')
+    }
 
-  /**
-   * Get month names in a year
-   */
-  public getMonthNames(): string[] {
-    return $dayjs.months().map((m) => capitalize(m))
-  }
+    /**
+     * Format date with the specified date format
+     *
+     * @param {DateInput} date Date
+     * @param {string} template Date format
+     */
+    public formatDate(date: DateInput, template: string): string {
+        return this._fixTzOffset(date).format(template)
+    }
 
-  /**
-   * Get week number
-   *
-   * If no @date parameter is specified the current week is returned
-   *
-   * @param {DateInput} date Optional date
-   */
-  public getWeek(date?: DateInput): number {
-    return $dayjs(date).isoWeek()
-  }
+    /**
+     * Get start of week
+     *
+     * @param {DateObject} date Date
+     * @param {boolean} isoWeek Use ISO week
+     */
+    public startOfWeek(date?: DateObject, isoWeek: boolean = this.$.isoWeek): DateObject {
+        return new DateObject(date.$.startOf(isoWeek ? 'isoWeek' : 'w'))
+    }
 
-  /**
-   * Get the month.
-   *
-   * Months are zero indexed, so January is month 0 and December is 11 (obviously).
-   *
-   * @param {DateInput} date Optional date
-   */
-  public getMonthIndex(date?: DateInput): number {
-    return $dayjs(date).month() + 1
-  }
+    /**
+     * Get end of week
+     *
+     * @param {DateObject} date Date
+     * @param {boolean} isoWeek Use ISO week
+     */
+    public endOfWeek(date?: DateObject, isoWeek: boolean = this.$.isoWeek): DateObject {
+        return new DateObject(date.$.endOf(isoWeek ? 'isoWeek' : 'w'))
+    }
 
-  /**
-   * Get the year
-   *
-   * If no @date parameter is specified the current year is returned
-   *
-   * @param {DateInput} date Optional date
-   */
-  public getYear(date?: DateInput): number {
-    return $dayjs(date).year()
-  }
+    /**
+     * Get days between a start and end time in the specified template
+     *
+     * @param {DateInput} start Start
+     * @param {DateInput} end End
+     * @param {string} template Date template
+     */
+    getDays(start: DateInput, end: DateInput, template: string = 'dddd DD'): string[] {
+        const days = []
+        let s = new DateObject(start)
+        const e = new DateObject(end)
+        while (s.isBeforeOrSame(e)) {
+            days.push(capitalize(s.format(template)))
+            s = s.add('1d')
+        }
+        return days
+    }
 
-  /**
-   * Is current week
-   *
-   * @param {DateObject} date Date
-   */
-  public isCurrentWeek(date: DateObject): boolean {
-    return date.$.week() === $dayjs().isoWeek()
-  }
+    /**
+     * Get month name for the speicifed month index
+     *
+     * @param {number} monthIndex Month index
+     * @param {string} template Template
+     */
+    public getMonthName(monthIndex?: number, template: string = this.$.monthFormat): string {
+        return $dayjs().set('month', monthIndex).format(template)
+    }
 
-  /**
-   * Is current year
-   *
-   * @param {DateObject} date Date
-   * @param {number} year Year
-   */
-  isCurrentYear(date: DateObject, year?: number) {
-    return (year || date.$.year()) === $dayjs().year()
-  }
+    /**
+     * Get timespan string
+     *
+     * @param {DateObject} start Start
+     * @param {DateObject} end End
+     * @param {string} monthFormat Month format
+     */
+    public getTimespanString(
+        start: DateObject,
+        end: DateObject,
+        monthFormat: string = this.$.monthFormat
+    ): string {
+        const isSameMonth = start.isSameMonth(end)
+        const isSameYear = start.isSameYear(end)
+        const sFormat = ['DD']
+        if (!isSameMonth) sFormat.push(monthFormat)
+        if (!isSameYear) sFormat.push('YYYY')
+        return [start.format(sFormat.join(' ')), end.format(`DD ${monthFormat} YYYY`)].join(' - ')
+    }
 
-  /**
-   * Is current year
-   *
-   * @param {DateInput} a Date a
-   * @param {DateInput} a Date b
-   */
-  isBefore(a: DateInput, b?: DateInput) {
-    return $dayjs(a).isBefore(b)
-  }
+    /**
+     * Get month names in a year
+     */
+    public getMonthNames(): string[] {
+        return $dayjs.months().map((m) => capitalize(m))
+    }
+
+    /**
+     * Get week number
+     *
+     * If no @date parameter is specified the current week is returned
+     *
+     * @param {DateInput} date Optional date
+     */
+    public getWeek(date?: DateInput): number {
+        return $dayjs(date).isoWeek()
+    }
+
+
+    /*
+     * Get Iso Week number
+     * Handles a weakness in dayjs, where week 53 occuring in january of a year
+     * e.g. jan 1-3 2021, is returned as january 2022
+     */
+    getIsoWeek(isoWeek: number, year: number) {
+        if (isoWeek === 53 && $dayjs().year(year - 1).isoWeeksInYear() === 53) return 0
+        return isoWeek
+    }
+
+    /**
+     * Get the month.
+     *
+     * Months are zero indexed, so January is month 0 and December is 11 (obviously).
+     *
+     * @param {DateInput} date Optional date
+     */
+    public getMonthIndex(date?: DateInput): number {
+        return $dayjs(date).month() + 1
+    }
+
+    /**
+     * Get the year
+     *
+     * If no @date parameter is specified the current year is returned
+     *
+     * @param {DateInput} date Optional date
+     */
+    public getYear(date?: DateInput): number {
+        return $dayjs(date).year()
+    }
+
+    /**
+     * Is current week
+     *
+     * @param {DateObject} date Date
+     */
+    public isCurrentWeek(date: DateObject): boolean {
+        return date.$.week() === $dayjs().isoWeek()
+    }
+
+    /**
+     * Is current year
+     *
+     * @param {DateObject} date Date
+     * @param {number} year Year
+     */
+    isCurrentYear(date: DateObject, year?: number) {
+        return (year || date.$.year()) === $dayjs().year()
+    }
+
+    /**
+     * Is current year
+     *
+     * @param {DateInput} a Date a
+     * @param {DateInput} a Date b
+     */
+    isBefore(a: DateInput, b?: DateInput) {
+        return $dayjs(a).isBefore(b)
+    }
 }
 
 export default new DateUtils({
-  tzOffset: new Date().getTimezoneOffset(),
-  monthFormat: 'MMMM',
-  isoWeek: true
+    tzOffset: new Date().getTimezoneOffset(),
+    monthFormat: 'MMMM',
+    isoWeek: true
 })
 
 export { DateObject, $dayjs }
