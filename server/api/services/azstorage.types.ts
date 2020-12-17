38ca6e636fbddbbceea2d408ecc733a2f857ce65
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-12-17 16:49:55 +0100

    Added AzTimeEntry.toEntity

--- server/api/services/azstorage.types.ts
+++ server/api/services/azstorage.types.ts
@@ -1,14 +1,54 @@
+import { pick } from 'underscore'
+import { getDurationHours } from '../../utils/date'
+import { convertToAzEntity, EntityDescriptor } from '../../utils/table'
 import MSGraphEvent from './msgraph.event'
 
 export type GetProjectsOptions = { noParse?: boolean; sortBy?: string }
 
 export class AzTimeEntry {
-    constructor(
-        public projectId: string,
-        public manualMatch: boolean,
-        public event: MSGraphEvent,
-        public labels: string[]
-    ) {
+  constructor(
+    public resourceId: string,
+    public periodId: string,
+    public projectId: string,
+    public manualMatch: boolean,
+    public event: MSGraphEvent,
+    public labels: string[]
+  ) {}
 
+  private get _period() {
+    const [weekNumber, monthNumber, year] = this.periodId.split('_').map((p) => parseInt(p, 10))
+    return {
+      periodId: this.periodId,
+      weekNumber,
+      monthNumber,
+      year
     }
+  }
+
+  public get duration(): number {
+    return getDurationHours(this.event.startDateTime, this.event.endDateTime)
+  }
+
+  public toEntity(): EntityDescriptor {
+    return convertToAzEntity(
+      `${this.periodId}_${this.event.id}`,
+      {
+        ...this._period,
+        ...pick(this.event, 'title', 'startDateTime', 'endDateTime', 'webLink'),
+        description: this.event.body,
+        projectId: this.projectId,
+        manualMatch: this.manualMatch,
+        duration: this.duration,
+        labels: this.labels.join('|')
+      },
+      this.resourceId,
+      {
+        removeBlanks: true,
+        typeMap: {
+          startDateTime: 'datetime',
+          endDateTime: 'datetime'
+        }
+      }
+    )
+  }
 }
