d3b84d5b30d7ec75b92601a0aa9fecc8afff203e
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-01-05 10:26:24 +0100

    DateRangePicker component

---
+++ client/pages/Admin/SummaryView/DateRangePicker/index.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { DatePicker, DayOfWeek, FirstWeekOfYear } from 'office-ui-fabric'
+import React, { useContext } from 'react'
+import { SummaryViewContext } from '../context'
+import { IDateRangePickerProps } from './types'
+import styles from './DateRangePicker.module.scss'
+import { DateObject } from 'DateUtils'
+
+export const DateRangePicker = (props: IDateRangePickerProps) => {
+    const context = useContext(SummaryViewContext)
+    // eslint-disable-next-line no-console
+    console.log(context)
+    return (
+        <div className={styles.root}>
+            <DatePicker
+                borderless={true}
+                showWeekNumbers={true}
+                firstDayOfWeek={DayOfWeek.Monday}
+                strings={context.t('common.calendarStrings', { returnObjects: true }) as any}
+                firstWeekOfYear={FirstWeekOfYear.FirstFourDayWeek}
+                value={context.range.from.jsDate}
+                onSelectDate={date => context.dispatch({
+                    type: 'SET_RANGE',
+                    payload: { from: new DateObject(date) },
+                })} />
+            <DatePicker
+                borderless={true}
+                showWeekNumbers={true}
+                firstDayOfWeek={DayOfWeek.Monday}
+                strings={context.t('common.calendarStrings', { returnObjects: true }) as any}
+                firstWeekOfYear={FirstWeekOfYear.FirstFourDayWeek}
+                value={context.range.to.jsDate}
+                onSelectDate={date => context.dispatch({
+                    type: 'SET_RANGE',
+                    payload: { to: new DateObject(date) },
+                })} />
+        </div>
+    )
+}
+
+export * from './types'
