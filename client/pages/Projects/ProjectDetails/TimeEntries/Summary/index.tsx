ed871a04dd5b9bd6ce7141ae5ef76c6538f34e90
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-02-04 10:15:46 +0100

    Added some styles..

--- client/pages/Projects/ProjectDetails/TimeEntries/Summary/index.tsx
+++ client/pages/Projects/ProjectDetails/TimeEntries/Summary/index.tsx
@@ -1,23 +1,21 @@
 import React, { FunctionComponent, useMemo } from 'react'
 import FadeIn from 'react-fade-in'
 import { useTranslation } from 'react-i18next'
-import { getSummary } from '../utils'
-import styles from './TimeEntries.module.scss'
+import { getSummary } from '../../utils'
+import styles from './Summary.module.scss'
 import { ISummaryProps } from './types'
 
 export const Summary: FunctionComponent<ISummaryProps> = ({ timeentries }: ISummaryProps) => {
   const { t } = useTranslation()
   const summary = useMemo(() => getSummary(timeentries, t), [timeentries])
   return (
-    <FadeIn className={styles.summary}>
-      <ul>
-        {summary.map(({ label, value }, idx) => (
-          <li key={idx}>
-            <span className={styles.label}>{label}</span>
-            <span className={styles.value}>{value}</span>
-          </li>
-        ))}
-      </ul>
+    <FadeIn className={styles.root}>
+      {summary.map(({ label, value }, idx) => (
+        <div key={idx} className={styles.item}>
+          <div className={styles.value}>{value}</div>
+          <div className={styles.label}>{label}</div>
+        </div>
+      ))}
     </FadeIn>
   )
 }
