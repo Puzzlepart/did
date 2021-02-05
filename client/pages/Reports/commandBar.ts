a3670560579d5ae94154ed62a4752933dc65cc28
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-02-05 11:45:08 +0100

    Added saveFilterCmd

--- client/pages/Reports/commandBar.ts
+++ client/pages/Reports/commandBar.ts
@@ -1,69 +1,91 @@
+import { AnyAction } from '@reduxjs/toolkit'
+import { TFunction } from 'i18next'
 import { format, IContextualMenuItem } from 'office-ui-fabric'
 import { pick } from 'underscore'
 import { exportExcel } from 'utils/exportExcel'
 import getColumns from './columns'
-import { SET_GROUP_BY, TOGGLE_FILTER_PANEL } from './reducer'
-import { getGroupByOptions } from './types'
+import { SET_FILTER, SET_GROUP_BY, TOGGLE_FILTER_PANEL } from './reducer'
+import { getGroupByOptions, IReportsState } from './types'
+
+interface IReportsCommmandParams {
+  state?: IReportsState
+  dispatch?: React.Dispatch<AnyAction>
+  t?: TFunction
+}
 
 /**
  * Select group by command
  */
-const selectGroupByCmd = ({ state, dispatch, t }) =>
-  !!state.query &&
-  !state.loading && {
-    key: 'SELECT_GROUP_BY',
-    text: t('common.groupBy'),
-    iconProps: { iconName: 'GroupList' },
-    subMenuProps: {
-      items: getGroupByOptions(t).map(
-        (opt) =>
-          ({
-            ...pick(opt, 'key', 'text'),
-            canCheck: true,
-            checked: state.groupBy.fieldName === opt.props.fieldName,
-            onClick: () => dispatch(SET_GROUP_BY({ groupBy: opt.props }))
-          } as IContextualMenuItem)
-      )
-    }
+const selectGroupByCmd = ({ state, dispatch, t }: IReportsCommmandParams) => ({
+  key: 'SELECT_GROUP_BY',
+  text: t('common.groupBy'),
+  iconProps: { iconName: 'GroupList' },
+  subMenuProps: {
+    items: getGroupByOptions(t).map(
+      (opt) =>
+      ({
+        ...pick(opt, 'key', 'text'),
+        canCheck: true,
+        checked: state.groupBy.fieldName === opt.props.fieldName,
+        onClick: () => dispatch(SET_GROUP_BY({ groupBy: opt.props }))
+      } as IContextualMenuItem)
+    )
   }
+})
 
 /**
  * Export to Excel command
  */
-const exportToExcelCmd = ({ state, t }) =>
-  !!state.query &&
-  !state.loading && {
-    key: 'EXPORT_TO_EXCEL',
-    text: t('reports.exportToExcel'),
-    onClick: () => {
-      const fileName = format(
-        state.query.exportFileName,
-        new Date().toDateString().split(' ').join('-')
-      )
-      exportExcel(state.subset, {
-        columns: getColumns({}, t),
-        fileName
-      })
-    },
-    iconProps: { iconName: 'ExcelDocument' }
-  }
+const exportToExcelCmd = ({ state, t }: IReportsCommmandParams) => ({
+  key: 'EXPORT_TO_EXCEL',
+  text: t('reports.exportToExcel'),
+  onClick: () => {
+    const fileName = format(
+      state.query.exportFileName,
+      new Date().toDateString().split(' ').join('-')
+    )
+    exportExcel(state.subset, {
+      columns: getColumns({}, t),
+      fileName
+    })
+  },
+  iconProps: { iconName: 'ExcelDocument' }
+})
 
 /**
  * Open filter panel command
  */
-const openFilterPanel = ({ state, dispatch }) =>
-  !!state.query &&
-  !state.loading && {
-    key: 'OPEN_FILTER_PANEL',
-    iconProps: { iconName: 'Filter' },
-    iconOnly: true,
-    onClick: () => dispatch(TOGGLE_FILTER_PANEL())
-  }
+const openFilterPanelCmd = ({ dispatch }: IReportsCommmandParams) => ({
+  key: 'OPEN_FILTER_PANEL',
+  iconProps: { iconName: 'Filter' },
+  iconOnly: true,
+  onClick: () => dispatch(TOGGLE_FILTER_PANEL())
+})
 
 /**
- * Command bar
+ * Save filter  command
  */
+const saveFilterCmd = ({ state, dispatch, t }: IReportsCommmandParams): IContextualMenuItem => ({
+  key: 'SAVED_FILTERS',
+  name: t('reports.savedFilters'),
+  subMenuProps: {
+    items: state.savedFilters.map((filter, idx) => ({
+      key: idx.toString(),
+      text: filter.name,
+      onClick: () => dispatch(SET_FILTER({ filter }))
+    }))
+  }
+})
+
 export default ({ state, dispatch, t }) => ({
-  items: [selectGroupByCmd({ state, dispatch, t })].filter((i) => i),
-  farItems: [exportToExcelCmd({ state, t }), openFilterPanel({ state, dispatch })].filter((i) => i)
+  items: (!!state.query && !state.loading)
+    ? [selectGroupByCmd({ state, dispatch, t })]
+    : [],
+  farItems: (!!state.query && !state.loading)
+    ? [
+      exportToExcelCmd({ state, t }),
+      openFilterPanelCmd({ dispatch }),
+      saveFilterCmd({ state, dispatch, t })
+    ]
+    : []
 })
