be94fbdf68936f67c22178fb128327a294dee9cd
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-02-05 11:45:21 +0100

    Updated ResourceFilter

--- client/pages/Reports/filters.ts
+++ client/pages/Reports/filters.ts
@@ -7,7 +7,11 @@ import {
 import { TFunction } from 'i18next'
 
 export const filters = (t: TFunction): BaseFilter[] => [
-  new ResourceFilter('resource.displayName', t('common.employeeLabel')),
+  new ResourceFilter(
+    'resource.id',
+    'resource.displayName',
+    t('common.employeeLabel')
+  ),
   new CustomerFilter('customer.name', t('common.customer')),
   new ProjectFilter('project.name', t('common.project'))
 ]
