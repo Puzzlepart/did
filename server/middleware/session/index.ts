e148d1977b8876eff334cc1604ec6994c08acf04
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-02-08 17:23:48 +0100

    Removed @types/connect-redis

--- server/middleware/session/index.ts
+++ server/middleware/session/index.ts
@@ -1,10 +1,9 @@
-/* eslint-disable no-console */
+
 /* eslint-disable @typescript-eslint/no-var-requires */
 import session from 'express-session'
 import { createClient as createRedisClient } from 'redis'
 import env from '../../utils/env'
-import ConnectRedis from 'connect-redis'
-const RedisStore = ConnectRedis(session)
+const RedisStore = require('connect-redis')(session)
 const client = createRedisClient(6380, env('REDIS_CACHE_HOSTNAME'), {
   auth_pass: env('REDIS_CACHE_KEY'),
   tls: {
@@ -12,6 +11,13 @@ const client = createRedisClient(6380, env('REDIS_CACHE_HOSTNAME'), {
   }
 })
 
+/**
+ * Defines session configuration; we use Redis for the session store.
+ * "secret" will be used to create the session ID hash (the cookie id and the redis key value)
+ * "name" will show up as your cookie name in the browser
+ * "cookie" is provided by default; you can add it to add additional personalized options
+ * The "store" ttl is the expiration time for each Redis session ID, in seconds
+ */
 export default session({
   name: env('SESSION_NAME', 'connect.sid'),
   store: new RedisStore({
