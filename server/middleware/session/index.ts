afc29ec57e5e9f4443717c4361f814567c56bc3e
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-02-08 14:45:46 +0100

    Session config for redis..

--- server/middleware/session/index.ts
+++ server/middleware/session/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-var-requires */
 import session from 'express-session'
 import redis from 'redis'
@@ -5,9 +6,16 @@ import env from '../../utils/env'
 const RedisStore = require('connect-redis')(session)
 const client = redis.createClient()
 
+client.on('error', (error) => {
+  console.error(error)
+})
+
+
 export default session({
   name: env('SESSION_NAME', 'connect.sid'),
   store: new RedisStore({ client }),
   secret: 'keyboard cat',
   resave: false,
+  saveUninitialized: false,
+  rolling: false
 })
