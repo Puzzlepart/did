a6f070f36c806b54ef6a212e1b4c8a3c9169a0ac
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-02-12 21:34:36 +0100

    Auth router

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,9 +1,6 @@
-import createDebug from 'debug'
 import { NextFunction, Request, Response, Router } from 'express'
 import passport from 'passport'
-import { SigninError, SIGNIN_FAILED } from '../middleware/passport/errors'
 import env from '../utils/env'
-const debug = createDebug('server/routes/auth')
 const auth = Router()
 
 auth.get('/signin', (request: Request, response: Response, next: NextFunction) => {
@@ -14,14 +11,12 @@ auth.get('/signin', (request: Request, response: Response, next: NextFunction) =
 })
 
 auth.post('/callback', (request: Request, response: Response, next: NextFunction) => {
-  passport.authenticate('azuread-openidconnect', (error: SigninError, user: Express.User) => {
+  passport.authenticate('azuread-openidconnect', (error: Error, user: Express.User) => {
     if (error) {
-      debug('Sign in failed with error code %s', error.code)
       return response.render('index', { error: error.toString() })
     }
     if (!user) {
-      debug('Sign in failed with error code %s', SIGNIN_FAILED.code)
-      return response.render('index', { error: SIGNIN_FAILED.toString() })
+      return response.render('index', { error: error.toString() })
     }
     request.logIn(user, (err) => {
       if (err) return response.render('index', { error: JSON.stringify(err) })
