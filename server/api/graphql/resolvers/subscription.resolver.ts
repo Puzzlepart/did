d83e26e6fd06649b9b8830b6a4f35fc9685dde2c
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-02-12 21:42:17 +0100

    Subscription resolver

--- server/api/graphql/resolvers/subscription.resolver.ts
+++ server/api/graphql/resolvers/subscription.resolver.ts
@@ -2,9 +2,9 @@
 /* eslint-disable require-await */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import 'reflect-metadata'
+import { MongoService } from 'server/api/services/mongo'
 import { Arg, Authorized, Ctx, Mutation, Query, Resolver } from 'type-graphql'
 import { Service } from 'typedi'
-import { pick } from 'underscore'
 import { IAuthOptions } from '../authChecker'
 import { Context } from '../context'
 import { Subscription, SubscriptionSettingsInput } from './subscription.types'
@@ -15,27 +15,22 @@ import { BaseResult } from './types'
 export class SubscriptionResolver {
   /**
    * Constructor for SubscriptionResolver
+   *
+   * @param {MongoService} _mongo Mongo service
    */
-  constructor() {}
+  constructor(private readonly _mongo: MongoService) { }
 
   /**
    * Get current subscription
    */
   @Authorized({ userContext: true })
-  @Query(() => Subscription, { description: 'Get current subscription' })
+  @Query(() => Subscription, { description: 'Get current subscription', nullable: true })
   async subscription(@Ctx() ctx: Context): Promise<Subscription> {
-    return await Promise.resolve(null)
-    // try {
-    //   return await this._subscription.getSubscription(ctx.subscription.id)
-    // } catch (error) {
-    //   return null
-    // }
+    return this._mongo.subscription.getById(ctx.subscription.id)
   }
 
   /**
    * Update subscription
-   *
-   * @permission MANAGE_SUBSCRIPTION (67ba6efc)
    *
    * @param {string} id Subscription ID
    * @param {SubscriptionSettingsInput} settings Settings
@@ -46,15 +41,9 @@ export class SubscriptionResolver {
     @Arg('id') id: string,
     @Arg('settings', () => SubscriptionSettingsInput) settings: SubscriptionSettingsInput
   ): Promise<BaseResult> {
-    return Promise.resolve({ success: true, error: null })
-    // try {
-    //   await this._subscription.updateSubscription<SubscriptionSettingsInput>(id, settings)
-    //   return { success: true, error: null }
-    // } catch (error) {
-    //   return {
-    //     success: false,
-    //     error: pick(error, 'name', 'message', 'code', 'statusCode')
-    //   }
-    // }
+    return Promise.resolve({
+      success: true,
+      error: null
+    })
   }
 }
--- server/api/services/mongo/subscription.ts
+++ server/api/services/mongo/subscription.ts
@@ -19,9 +19,23 @@ export class SubscriptionMongoService {
    *
    * @param {string} id Subscription ID
    */
-  public async getById(id: string) {
+  public async getById(id: string): Promise<Subscription> {
     try {
       const result = await this._collection.findOne({ id })
+      return result
+    } catch (err) {
+      throw err
+    }
+  }
+
+  /**
+   * Add subscription
+   *
+   * @param {Subscription} subscription Subscription
+   */
+  public async addSubscription(subscription: Subscription) {
+    try {
+      const result = await this._collection.insertOne(subscription)
       return result
     } catch (err) {
       throw err
