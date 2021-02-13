3459aad44a8cdadc3305b57b2245b13796331267
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-02-13 13:52:52 +0100

    Projects.getProjects includes customer

--- server/api/services/mongo/project.ts
+++ server/api/services/mongo/project.ts
@@ -1,9 +1,12 @@
 import * as Mongo from 'mongodb'
+import { CustomerMongoService } from './'
 import { Project } from '../../graphql/resolvers/types'
+import { find } from 'underscore'
 
 export class ProjectMongoService {
   private _collectionName = 'projects'
   private _collection: Mongo.Collection<Project>
+  private _customer: CustomerMongoService
   /**
    * Constructor
    *
@@ -11,5 +14,32 @@ export class ProjectMongoService {
    */
   constructor(db: Mongo.Db) {
     this._collection = db.collection(this._collectionName)
+    this._customer = new CustomerMongoService(db)
+  }
+
+  /**
+   * Get projects
+   *
+   * @param {Mongo.FilterQuery<Project>} query Query
+   */
+  public async getProjects(query?: Mongo.FilterQuery<Project>): Promise<Project[]> {
+    try {
+      // eslint-disable-next-line prefer-const
+      let [projects, customers] = await Promise.all([
+        this._collection.find(query).toArray(),
+        this._customer.getCustomers()
+      ])
+      projects = projects
+        .map(p => {
+          p.customer = find(customers, c => c.key === p.customerKey) || null
+          // TODO: Set labels using LabelMongoServide
+          p.labels = []
+          return p
+        })
+        .filter(p => p.customer !== null)
+      return projects
+    } catch (err) {
+      throw err
+    }
   }
 }
