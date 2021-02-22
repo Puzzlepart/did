6080dcda9f8112045617d949038dee3495f6f8d7
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-02-22 15:39:19 +0100

    Updated NotificationService

--- server/services/notification/index.ts
+++ server/services/notification/index.ts
@@ -1,16 +1,18 @@
-import { Db } from 'mongodb'
+import get from 'get-value'
+import { Collection } from 'mongodb'
 import 'reflect-metadata'
-import format from 'string-format'
 import { Inject, Service } from 'typedi'
 import { find } from 'underscore'
-import { DateObject } from '../../shared/utils/date'
-import { Context } from '../graphql/context'
-import { NotificationTemplates } from '../graphql/resolvers/types'
-import { TimesheetService } from './timesheet'
+import { DateObject } from '../../../shared/utils/date'
+import { Context } from '../../graphql/context'
+import { NotificationTemplates } from '../../graphql/resolvers/types'
+import { TimesheetService } from '../timesheet'
+import { ForecastNotification, UnconfirmedPeriodNotification } from './types'
 
 @Service({ global: false })
 export class NotificationService {
-  private _db: Db
+  private _confirmed_periods: Collection
+  private _forecasted_periods: Collection
   /**
    * Constructor
    *
@@ -21,7 +23,9 @@ export class NotificationService {
     @Inject('CONTEXT') private readonly context: Context,
     private readonly _timesheet: TimesheetService
   ) {
-    this._db = this.context.client.db('test')
+    const db = this.context.client.db('test')
+    this._confirmed_periods = db.collection('confirmed_periods')
+    this._forecasted_periods = db.collection('forecasted_periods')
   }
 
   /**
@@ -42,7 +46,7 @@ export class NotificationService {
       currentDate = currentDate.add('-1w')
     }
 
-    const confirmedPeriods = await this._db.collection('confirmed_periods').find({
+    const confirmedPeriods = await this._confirmed_periods.find({
       userId: this.context.userId
     }).toArray()
 
@@ -52,13 +56,10 @@ export class NotificationService {
       }
     })
 
-    return unconfirmedPeriods.map((period) => ({
-      id: `unconfirmed_period_${period.id}`,
-      type: 0,
-      severity: 2,
-      text: format(template, period.week, period.month),
-      moreLink: ['', 'timesheet/overview', ...period.id.split('_')].join('/')
-    }))
+    return unconfirmedPeriods.map((period) => new UnconfirmedPeriodNotification(
+      period,
+      template
+    ))
   }
 
   /**
@@ -68,9 +69,35 @@ export class NotificationService {
    * @param {string} locale Locale
    */
   private async _forecast(template: string, locale: string) {
-    // eslint-disable-next-line no-console
-    console.log({ template, locale })
-    return await Promise.resolve([])
+    if (!get(this.context, 'subscription.settings.forecast.enabled', { default: false })) return []
+    const periods = []
+    const unforecastedPeriods = []
+    let currentDate = new DateObject().add('1w')
+
+    for (
+      let i = 0;
+      i < get(this.context, 'subscription.settings.forecast.notifications', { default: 2 });
+      i++
+    ) {
+      const startOfWeek = currentDate.startOfWeek.format('YYYY-MM-DD')
+      const endOfWeek = currentDate.endOfWeek.format('YYYY-MM-DD')
+      periods.push(...this._timesheet.getPeriods(startOfWeek, endOfWeek, locale))
+      currentDate = currentDate.add('1w')
+    }
+
+    const forecastedPeriods = await this._forecasted_periods.find({
+      userId: this.context.userId
+    }).toArray()
+
+    periods.forEach((period) => {
+      if (!find(forecastedPeriods, ({ id }) => id === period.id))
+        unforecastedPeriods.push(period)
+    })
+
+    return unforecastedPeriods.map((period) => new ForecastNotification(
+      period,
+      template
+    ))
   }
 
   public async getNotifications(templates: NotificationTemplates, locale: string) {
