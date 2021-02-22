125456607120655545555f0f2ee3dd88e36b0d02
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-02-22 12:37:41 +0100

    Removed mocha

--- server/graphql/resolvers/user/index.ts
+++ server/graphql/resolvers/user/index.ts
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Context } from 'mocha'
 import 'reflect-metadata'
 import { Arg, Authorized, Ctx, Mutation, Query, Resolver } from 'type-graphql'
 import { Service } from 'typedi'
@@ -7,6 +6,7 @@ import { pick } from 'underscore'
 import { MSGraphService } from '../../../services'
 import { MongoService } from '../../../services/mongo'
 import { IAuthOptions } from '../../authChecker'
+import { Context } from '../../context'
 import { BaseResult } from '../types'
 import { User, UserInput, UserQuery, UserQueryOptions } from './types'
 
