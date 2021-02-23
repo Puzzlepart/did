c9c06a97aa042249f5891cd2fd2b0c4040030bed
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-02-23 18:25:59 +0100

    Adjustments to ProjectResolver

--- server/graphql/resolvers/project/index.ts
+++ server/graphql/resolvers/project/index.ts
@@ -1,12 +1,10 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import 'reflect-metadata'
 import { Arg, Authorized, Mutation, Query, Resolver } from 'type-graphql'
 import { Service } from 'typedi'
 import { MongoService } from '../../../services/mongo'
 import MSGraphService from '../../../services/msgraph'
 import { IAuthOptions } from '../../authChecker'
-import { BaseResult, Project, ProjectInput, ProjectOptions } from '../types'
+import { Project, ProjectInput, ProjectOptions } from '../types'
 
 @Service()
 @Resolver(Project)
@@ -17,22 +15,20 @@ export class ProjectResolver {
    * @param {MongoService} _mongo Mongo service
    * @param {MSGraphService} _msgraph MSGraphService
    */
-  constructor(private readonly _mongo: MongoService, private readonly _msgraph: MSGraphService) {}
+  constructor(private readonly _mongo: MongoService, private readonly _msgraph: MSGraphService) { }
 
   /**
    * Get projects
    *
    * @param {string} customerKey Customer key
-   * @param {string} sortBy Sort by
    */
   @Authorized<IAuthOptions>()
   @Query(() => [Project], { description: 'Get projects' })
   async projects(
-    @Arg('customerKey', { nullable: true }) customerKey: string,
-    @Arg('sortBy', { nullable: true }) sortBy: string
+    @Arg('customerKey', { nullable: true }) customerKey: string
   ): Promise<Project[]> {
-    const data = await this._mongo.project.getProjectsData(customerKey && { customerKey })
-    return data.projects
+    const { projects } = await this._mongo.project.getProjectsData(customerKey && { customerKey })
+    return projects
   }
 
   /**
@@ -43,19 +39,19 @@ export class ProjectResolver {
    * @param {boolean} update Update
    */
   @Authorized<IAuthOptions>({ permission: 'ef4032fb' })
-  @Mutation(() => BaseResult, { description: 'Create or update project' })
+  @Mutation(() => Boolean, { description: 'Create or update project' })
   async createOrUpdateProject(
     @Arg('project', () => ProjectInput) project: ProjectInput,
     @Arg('options', () => ProjectOptions) options: ProjectOptions,
     @Arg('update', { nullable: true }) update: boolean
-  ): Promise<BaseResult> {
+  ): Promise<boolean> {
     const p = new Project().fromInput(project)
     if (options.createOutlookCategory) {
       await this._msgraph.createOutlookCategory(p.tag)
     }
     if (update) await this._mongo.project.updateProject(p)
     else await this._mongo.project.addProject(p)
-    return { success: true, error: null }
+    return true
   }
 }
 
