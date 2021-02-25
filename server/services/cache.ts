47f20472496d3083fbb1ac84e7ecabf4a9bdd194
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-02-25 13:44:25 +0100

    Logging in CacheService

--- server/services/cache.ts
+++ server/services/cache.ts
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable max-classes-per-file */
 import 'reflect-metadata'
 import { Inject, Service } from 'typedi'
 import { filter, isArray } from 'underscore'
 import { Context } from '../graphql/context'
 import Redis from '../middleware/redis'
+const log = require('debug')('server/services/cache')
 
 export enum CacheScope {
   USER,
@@ -25,7 +27,7 @@ export class CacheService {
     @Inject('CONTEXT') private readonly context: Context,
     public prefix?: string,
     public scope: CacheScope = CacheScope.SUBSCRIPTION
-  ) {}
+  ) { }
 
   /**
    * Get scoped cache key
@@ -52,14 +54,22 @@ export class CacheService {
   /**
    * Get from cache by key
    *
-   * @param {sCacheKey} key Cache key
+   * @param {CacheKey} key Cache key
    * @param {CacheScope} scope Cache scope
    */
   public get<T = any>(key: CacheKey, scope: CacheScope = this.scope): Promise<T> {
     return new Promise((resolve) => {
-      Redis.get(this._getScopedCacheKey(key, scope), (err, reply) => {
-        if (err) resolve(null)
-        else resolve(JSON.parse(reply) as T)
+      const scopedCacheKey = this._getScopedCacheKey(key, scope)
+      log(`Retrieving cached value for key ${scopedCacheKey}...`)
+      Redis.get(scopedCacheKey, (err, reply) => {
+        if (err) {
+          log(`Failed to retrieve cachedd value for key ${scopedCacheKey}.`)
+          resolve(null)
+        }
+        else {
+          log(`Retrieved cached value for key ${scopedCacheKey}.`)
+          resolve(JSON.parse(reply) as T)
+        }
       })
     })
   }
@@ -74,7 +84,22 @@ export class CacheService {
    */
   public set(key: CacheKey, value: any, seconds: number = 60, scope: CacheScope = this.scope) {
     return new Promise((resolve) => {
-      Redis.setex(this._getScopedCacheKey(key, scope), seconds, JSON.stringify(value), resolve)
+      const scopedCacheKey = this._getScopedCacheKey(key, scope)
+      log(`Setting value for key ${scopedCacheKey} with a expiration of ${seconds} seconds...`)
+      Redis.setex(
+        scopedCacheKey,
+        seconds,
+        JSON.stringify(value),
+        (err, reply) => {
+          if (err) {
+            log(`Failed to set value for key ${scopedCacheKey}.`)
+            resolve(err)
+          }
+          else {
+            log(`Value for key ${scopedCacheKey} set with a expiration of ${seconds} seconds.`)
+            resolve(reply)
+          }
+        })
     })
   }
 
