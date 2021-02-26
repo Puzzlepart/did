e3f6ebfe3a6a7ca858c0dcc5e07541ba55edbfe4
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-02-26 15:25:56 +0100

    Timesheet resolver types in separate folder

--- server/graphql/resolvers/timesheet/index.ts
+++ server/graphql/resolvers/timesheet/index.ts
@@ -4,12 +4,7 @@ import { Service } from 'typedi'
 import { TimesheetService } from '../../../services'
 import { IAuthOptions } from '../../authChecker'
 import { BaseResult } from '../types'
-import {
-  TimesheetOptions,
-  TimesheetPeriodInput,
-  TimesheetPeriodObject,
-  TimesheetQuery
-} from './types'
+import { TimesheetOptions, TimesheetPeriodInput, TimesheetPeriodObject, TimesheetQuery } from './types'
 
 @Service()
 @Resolver(TimesheetPeriodObject)
