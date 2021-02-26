b34201c4243a3852b6c5a641a751c9f909b308ef
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-02-26 11:19:15 +0100

    react-hooks/exhaustive-deps

--- client/pages/Timesheet/AllocationView/useChartConfig.ts
+++ client/pages/Timesheet/AllocationView/useChartConfig.ts
@@ -6,28 +6,31 @@ import { IChartConfig } from './types'
  * Hook for chart configuration
  */
 export function useChartConfig(): IChartConfig[] {
-    const { t } = useTranslation()
-    return useMemo(() => ([
-        {
-            key: 'project',
-            title: t('timesheet.allocation.projectChartTitle'),
-            subTitle: t('timesheet.allocation.projectChartDescription'),
-            colors: 'light',
-            idKey: 'name',
-            valueKey: 'duration',
-            valuePostfix: t('common.hours'),
-            textKey: 'name',
-            subTextKey: 'customer.name'
-        },
-        {
-            key: 'customer',
-            title: t('timesheet.allocation.customerChartTitle'),
-            subTitle: t('timesheet.allocation.customerChartDescription'),
-            colors: 'light',
-            idKey: 'name',
-            valueKey: 'duration',
-            valuePostfix: t('common.hours'),
-            textKey: 'name'
-        }
-    ]), [t])
+  const { t } = useTranslation()
+  return useMemo(
+    () => [
+      {
+        key: 'project',
+        title: t('timesheet.allocation.projectChartTitle'),
+        subTitle: t('timesheet.allocation.projectChartDescription'),
+        colors: 'light',
+        idKey: 'name',
+        valueKey: 'duration',
+        valuePostfix: t('common.hours'),
+        textKey: 'name',
+        subTextKey: 'customer.name'
+      },
+      {
+        key: 'customer',
+        title: t('timesheet.allocation.customerChartTitle'),
+        subTitle: t('timesheet.allocation.customerChartDescription'),
+        colors: 'light',
+        idKey: 'name',
+        valueKey: 'duration',
+        valuePostfix: t('common.hours'),
+        textKey: 'name'
+      }
+    ],
+    [t]
+  )
 }
