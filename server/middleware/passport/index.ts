28ed0f4b3b0c4341f8ca24fe5f764a93a37e978d
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-03-16 18:17:25 +0100

    chore: googleStrategy in passport

--- server/middleware/passport/index.ts
+++ server/middleware/passport/index.ts
@@ -3,8 +3,9 @@ import fs from 'fs'
 import { MongoClient } from 'mongodb'
 import passport from 'passport'
 import { IProfile, OIDCStrategy, VerifyCallback } from 'passport-azure-ad'
+import { Strategy as GoogleStrategyOAuth2 } from 'passport-google-oauth20'
 import { environment } from '../../utils'
-import { onVerifySignin } from './onVerifySignin'
+import { onVerifySigninGoogle, onVerifySigninMicrosoft } from './onVerifySignin'
 
 /**
  * Get redirect URL
@@ -38,23 +39,7 @@ export const passportMiddleware = (mongoClient: MongoClient) => {
   passport.serializeUser((user, done) => done(null, user))
   passport.deserializeUser((user, done) => done(null, user))
 
-  /**
-   * OIDCStrategy uses OpenID Connect protocol for web application login purposes.
-   * It works in the following manner:
-   * If a user is not logged in, passport sends an authentication request to
-   * AAD (Azure Active Directory), and AAD prompts the user for his or her sign-in
-   * credentials. On successful authentication, depending on the flow you choose,
-   * web application will eventually get an id_token back either directly
-   * from the AAD authorization endpoint or by redeeming a code at the AAD
-   * token endpoint. Passport then validates the id_token and propagates
-   * the claims in id_token back to the verify callback, and let the framework
-   * finish the remaining authentication procedure. If the whole process is
-   * successful, passport adds the user information to req.user and passes it
-   * to the next middleware. In case of error, passport either sends back an
-   * unauthorized response or redirects the user to the page you specified
-   * (such as homepage or login page).
-   */
-  const strategy = () => {
+  const azureAdStrategy = () => {
     const redirectUrl = getRedirectUrl()
     return new OIDCStrategy(
       {
@@ -78,10 +63,24 @@ export const passportMiddleware = (mongoClient: MongoClient) => {
         _refreshToken: string,
         tokenParameters: any,
         done: VerifyCallback
-      ) => onVerifySignin(mongoClient, profile, tokenParameters, done)
+      ) => onVerifySigninMicrosoft(mongoClient, profile, tokenParameters, done)
     )
   }
 
-  passport.use(strategy())
+  const googleStrategy = () => {
+    return new GoogleStrategyOAuth2(
+      {
+        clientID: environment('GOOGLE_CLIENT_ID'),
+        clientSecret: environment('GOOGLE_CLIENT_SECRET'),
+        callbackURL: environment('GOOGLE_REDIRECT_URI')
+      },
+      (accessToken, _refreshToken, profile, done) =>
+        onVerifySigninGoogle(mongoClient, accessToken, profile, done)
+    )
+  }
+
+  passport.use(azureAdStrategy())
+  passport.use(googleStrategy())
+
   return passport
 }
