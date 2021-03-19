f9a8d447e0abb7ff1b4ff1e7817825359d09e8a0
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-03-19 11:36:44 +0100

    chore: profile sync adjustments

--- server/middleware/passport/microsoft/synchronizeUserProfile.ts
+++ server/middleware/passport/microsoft/synchronizeUserProfile.ts
@@ -1,21 +1,22 @@
 import createDebug from 'debug'
-import { UserPhoto } from 'server/graphql'
 import { isEmpty, isEqual, pick } from 'underscore'
+import { User } from '../../../graphql'
 import { MSGraphService, UserService } from '../../../services'
 import OAuthService from '../../../services/oauth'
 const debug = createDebug('middleware/passport/synchronizeUserProfile')
 
 /**
- * Synchronize user profile
+ * Synchronize user profile properties and user photo based
+ * on subscription settings.
  *
  * @param user - User
- * @param userSrv - User service
+ * @param userSvc - User service
  */
 export async function synchronizeUserProfile(
-  user: Express.User,
-  userSrv: UserService
+  user: User,
+  userSvc: UserService
 ): Promise<void> {
-  const properties = user?.subscription?.settings?.adsync?.properties || []
+  const { properties, syncUserPhoto } = user?.subscription?.settings?.adsync
   if (isEmpty(properties)) {
     debug(
       'User profile synchronization is turned on, but no properties are selected.'
@@ -26,13 +27,17 @@ export async function synchronizeUserProfile(
     msgraphSrv.getCurrentUser(properties),
     msgraphSrv.getUserPhoto('48x48')
   ])
-  const photo: UserPhoto = {
-    base64: photoBase64
-  }
-  const needSync = !isEqual(pick(user, ...properties), {
-    photo,
-    ...pick(data, ...properties)
+  const needSync = !isEqual(pick(user, [...properties, 'photo']), {
+    photo: {
+      base64: photoBase64
+    },
+    ...pick(data, [...properties, 'photo'])
   })
+  if (syncUserPhoto) {
+    user.photo = {
+      base64: photoBase64
+    }
+  }
   if (!needSync) {
     debug('User profile properties are up to date!')
     return
@@ -41,10 +46,9 @@ export async function synchronizeUserProfile(
     'Synchronizing user profile properties %s from Azure AD.',
     properties.join(', ')
   )
-  await userSrv.updateUser({
-    id: user.id,
-    ...pick(data, properties),
-    photo
+  await userSvc.updateUser({
+    ...pick(user, 'id', 'photo'),
+    ...pick(data, [...properties, 'photo'])
   })
   debug('User profile properties synchronized from Azure AD.')
 }
