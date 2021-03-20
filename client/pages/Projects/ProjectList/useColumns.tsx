52b1e652518b8df1850ada508754e2676e4cb326
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-03-20 23:22:05 +0100

    build: test

--- client/pages/Projects/ProjectList/useColumns.tsx
+++ client/pages/Projects/ProjectList/useColumns.tsx
@@ -1,13 +1,21 @@
+/* eslint-disable tsdoc/syntax */
 import { IconText } from 'components'
 import { CustomerLink } from 'components/CustomerLink'
 import { EntityLabel } from 'components/EntityLabel'
 import { IListColumn } from 'components/List/types'
 import * as React from 'react'
 import { useTranslation } from 'react-i18next'
-import { LabelObject as Label, Project } from 'types'
+import { LabelObject, Project } from 'types'
 import { generateColumn as col } from 'utils/generateColumn'
 import { NameLabel } from './NameLabel'
 import { IProjectListProps } from './types'
+
+/**
+ * @ignore
+ */
+const ColumnWrapper = ({ project, children }) => (
+  <div style={{ opacity: project.inactive ? 0.4 : 1 }}>{children}</div>
+)
 
 /**
  * Returns column definitions
@@ -15,25 +23,37 @@ import { IProjectListProps } from './types'
 export function useColumns(props: IProjectListProps): IListColumn[] {
   const { t } = useTranslation()
   return [
-    col('key', '', { minWidth: 125, maxWidth: 125 }, (project: Project) => {
-      if (project.inactive) {
-        return (
-          <IconText
-            title={t('projects.inactiveText')}
-            iconName='Warning'
-            styles={{ root: { color: '#ffbf00' } }}
-            text={project.key}
-          />
-        )
+    col(
+      'key',
+      '',
+      {
+        minWidth: 125,
+        maxWidth: 125
+      },
+      (project: Project) => {
+        if (project.inactive) {
+          return (
+            <ColumnWrapper project={project}>
+              <IconText
+                title={t('projects.inactiveText')}
+                iconName='Warning'
+                styles={{ root: { color: '#ffbf00' } }}
+                text={project.key}
+              />
+            </ColumnWrapper>
+          )
+        }
+        return <IconText iconName={project.icon} text={project.key} />
       }
-      return <IconText iconName={project.icon} text={project.key} />
-    }),
+    ),
     col(
       'name',
       t('common.nameFieldLabel'),
       { maxWidth: 220 },
       (project: Project) => (
-        <NameLabel project={project} renderLink={props.renderLink} />
+        <ColumnWrapper project={project}>
+          <NameLabel project={project} renderLink={props.renderLink} />
+        </ColumnWrapper>
       )
     ),
     col(
@@ -42,13 +62,19 @@ export function useColumns(props: IProjectListProps): IListColumn[] {
       { maxWidth: 220 },
       (project: Project) => {
         if (!project.customer) return null
-        return <CustomerLink customer={project.customer} />
+        return (
+          <ColumnWrapper project={project}>
+            <CustomerLink customer={project.customer} />
+          </ColumnWrapper>
+        )
       }
     ),
-    col('labels', '', {}, (project: Project) =>
-      (project.labels as Label[]).map((label, index) => (
-        <EntityLabel key={index} label={label} />
-      ))
-    )
+    col('labels', '', {}, (project) => (
+      <ColumnWrapper project={project}>
+        {project.labels.map((label: LabelObject, index: number) => (
+          <EntityLabel key={index} label={label} />
+        ))}
+      </ColumnWrapper>
+    ))
   ].filter((col) => !(props.hideColumns || []).includes(col.key))
 }
