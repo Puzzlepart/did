92904c05d323ee80ed630b4866c4525e1fd49e61
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-03-24 14:05:00 +0100

    chore: updated feedback panel

--- client/app/Navigation/UserFeedback/FeedbackPanel/index.tsx
+++ client/app/Navigation/UserFeedback/FeedbackPanel/index.tsx
@@ -1,7 +1,9 @@
 /* eslint-disable tsdoc/syntax */
+import { Toast } from 'components'
 import { ChoiceGroup, DefaultButton, Dropdown, IPanelProps, Panel, PanelType, PrimaryButton, TextField } from 'office-ui-fabric-react'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import { omit } from 'underscore'
 import styles from './FeedbackPanel.module.scss'
 import { useFeedbackModel } from './useFeedbackModel'
 import { useSubmitFeedback } from './useSubmitFeedback'
@@ -20,51 +22,57 @@ export const FeedbackPanel: React.FC<IPanelProps> = (props) => {
     typeOptions,
     moodOptions
   } = useFeedbackModel()
-  const submitProps = useSubmitFeedback(model, props)
+  const submit = useSubmitFeedback(model, props)
   return (
-    <Panel
-      isOpen={props.isOpen}
-      className={styles.root}
-      headerText={t('feedback.headerText')}
-      type={PanelType.smallFixedFar}
-      isLightDismiss={true}
-      onDismiss={props.onDismiss}>
-      <div className={styles.body}>
-        <Dropdown
-          label={t('feedback.typeFieldLabel')}
-          required={true}
-          defaultSelectedKey={model.labels[0]}
-          options={typeOptions}
-          onChange={(_event, option) => setType(option.key as string)} />
-        <TextField
-          label={t('feedback.summaryFieldLabel')}
-          required={true}
-          value={model.title}
-          onChange={(_event, title) => setTitle(title)}
-        />
-        <TextField
-          label={t('feedback.descriptionFieldLabel')}
-          description={t('feedback.descriptionFieldDesc')}
-          multiline={true}
-          required={true}
-          autoAdjustHeight={true}
-          styles={{ field: { height: 200 } }}
-          value={model.body}
-          onChange={(_event, body) => setBody(body)}
-        />
-        <ChoiceGroup
-          label={t('feedback.ratingFieldLabel')}
-          required={true}
-          defaultSelectedKey={model.mood}
-          onChange={(_event, option) => setMood(option.key)}
-          options={moodOptions} />
-      </div>
-      <div className={styles.footer}>
-        <PrimaryButton
-        {...submitProps}
-         text={t('feedback.submitButtonText')}  />
-        <DefaultButton text={t('feedback.cancelButtonLabel')} style={{ marginLeft: 8 }} />
-      </div>
-    </Panel>
+    <>
+      <Panel
+        isOpen={props.isOpen}
+        className={styles.root}
+        headerText={t('feedback.headerText')}
+        type={PanelType.smallFixedFar}
+        isLightDismiss={true}
+        onDismiss={props.onDismiss}>
+        <div className={styles.body}>
+          <Dropdown
+            label={t('feedback.typeFieldLabel')}
+            required={true}
+            defaultSelectedKey={model.labels[0]}
+            options={typeOptions}
+            onChange={(_event, option) => setType(option.key as string)} />
+          <TextField
+            label={t('feedback.summaryFieldLabel')}
+            required={true}
+            value={model.title}
+            onChange={(_event, title) => setTitle(title)}
+          />
+          <TextField
+            label={t('feedback.descriptionFieldLabel')}
+            description={t('feedback.descriptionFieldDesc')}
+            multiline={true}
+            required={true}
+            autoAdjustHeight={true}
+            styles={{ field: { height: 200 } }}
+            value={model.body}
+            onChange={(_event, body) => setBody(body)}
+          />
+          <ChoiceGroup
+            label={t('feedback.ratingFieldLabel')}
+            required={true}
+            defaultSelectedKey={model.mood}
+            onChange={(_event, option) => setMood(option.key)}
+            options={moodOptions} />
+        </div>
+        <div className={styles.footer}>
+          <PrimaryButton
+            {...omit(submit, 'toast')}
+            text={t('feedback.submitButtonText')} />
+          <DefaultButton
+            text={t('feedback.cancelButtonLabel')}
+            onClick={() => props.onDismiss()}
+            style={{ marginLeft: 8 }} />
+        </div>
+      </Panel>
+      <Toast {...submit.toast} />
+    </>
   )
 }
--- client/app/Navigation/UserFeedback/FeedbackPanel/useSubmitFeedback.ts
+++ client/app/Navigation/UserFeedback/FeedbackPanel/useSubmitFeedback.ts
@@ -1,14 +1,23 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useMutation } from '@apollo/client'
+import { IUserMessageProps } from 'components'
+import { useToast } from 'components/Toast'
 import { IPanelProps } from 'office-ui-fabric-react'
 import { IButtonProps } from 'office-ui-fabric-react/lib/Button'
 import { useCallback } from 'react'
+import { useTranslation } from 'react-i18next'
 import { UserFeedback } from 'types'
 import { isEmpty } from 'underscore'
 import $submit_feedback from './submit-feedback.gql'
 
-export const useSubmitFeedback = (feedback: UserFeedback, panel: IPanelProps): IButtonProps => {
+export interface UseSubmitFeedbackReturnType extends IButtonProps {
+    toast: IUserMessageProps
+}
+
+export const useSubmitFeedback = (feedback: UserFeedback, panel: IPanelProps): UseSubmitFeedbackReturnType => {
+    const { t } = useTranslation()
     const [submitFeedback] = useMutation($submit_feedback)
+    const [toast, setToast] = useToast(8000)
 
     /**
       * On submit feedback
@@ -17,12 +26,14 @@ export const useSubmitFeedback = (feedback: UserFeedback, panel: IPanelProps): I
         const { data } = await submitFeedback({
             variables: { feedback }
         })
-        return data
+        return data.result
     }, [feedback])
 
     return {
+        toast,
         onClick: async () => {
-            await onSubmitFeedback()
+            const result = await onSubmitFeedback()
+            setToast({ text: t('feedback.submitSuccessMessage', result) })
             panel.onDismiss()
         },
         disabled: isEmpty(feedback.title) || isEmpty(feedback.body) || !feedback.mood
