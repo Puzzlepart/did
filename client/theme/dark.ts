21b845c5b171ca70df854a244e16b6f41b3a50ab
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-03-25 12:16:40 +0100

    chore: adjusted dark theme, ref https://github.com/microsoft/fluentui/issues/9795

--- client/theme/dark.ts
+++ client/theme/dark.ts
@@ -2,27 +2,18 @@ import { Theme } from '@fluentui/react'
 
 export default {
     palette: {
-      themePrimary: '#0078d4',
-      themeLighterAlt: '#eff6fc',
-      themeLighter: '#deecf9',
-      themeLight: '#c7e0f4',
-      themeTertiary: '#71afe5',
-      themeSecondary: '#2b88d8',
-      themeDarkAlt: '#106ebe',
-      themeDark: '#005a9e',
-      themeDarker: '#004578',
-      neutralLighterAlt: '#0b0b0b',
-      neutralLighter: '#151515',
-      neutralLight: '#252525',
-      neutralQuaternaryAlt: '#2f2f2f',
-      neutralQuaternary: '#373737',
-      neutralTertiaryAlt: '#595959',
-      neutralTertiary: '#fcf7f7',
-      neutralSecondary: '#fcf8f8',
-      neutralPrimaryAlt: '#fdf9f9',
-      neutralPrimary: '#f9f2f2',
-      neutralDark: '#fefcfc',
-      black: '#fefdfd',
-      white: '#000000'
+      neutralLighterAlt: '#282828',
+      neutralLighter: '#313131',
+      neutralLight: '#3f3f3f',
+      neutralQuaternaryAlt: '#484848',
+      neutralQuaternary: '#4f4f4f',
+      neutralTertiaryAlt: '#6d6d6d',
+      neutralTertiary: '#c8c8c8',
+      neutralSecondary: '#d0d0d0',
+      neutralPrimaryAlt: '#dadada',
+      neutralPrimary: '#ffffff',
+      neutralDark: '#f4f4f4',
+      black: '#f8f8f8',
+      white: '#1f1f1f'
     }
   } as Theme
