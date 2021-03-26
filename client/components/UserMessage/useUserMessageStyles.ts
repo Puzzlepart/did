51d6fc13ddb335233c2a9eb176d8edb1c0575bee
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-03-26 16:31:47 +0100

    docs: commented useusermessagestyles (not obvious) 📝

--- client/components/UserMessage/useUserMessageStyles.ts
+++ client/components/UserMessage/useUserMessageStyles.ts
@@ -3,6 +3,14 @@ import { makeStyles } from '@fluentui/react'
 import { UserMessageType } from './types'
 
 /**
+ * We use `makeStyles` to get our `background` and `color`
+ * styles. 
+ * 
+ * `theme.semanticColors` contains variables for all types of 
+ * messages in the format `successText`, `successIcon` and 
+ * `successBackground`. We dynamically get these variables (colors)
+ * with our type (`UserMessageType`)
+ * 
  * @category UserMessage
  */
 export function useUserMessageStyles(type: UserMessageType) {
