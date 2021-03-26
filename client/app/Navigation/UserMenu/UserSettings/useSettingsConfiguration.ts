e09085fdb990bce6fde3c2fafe552d62a3197f5e
Kim Damsleth <kim@damsleth.com>
2021-03-26 08:59:05 +0100

    chore: hide theme selector

--- client/app/Navigation/UserMenu/UserSettings/useSettingsConfiguration.ts
+++ client/app/Navigation/UserMenu/UserSettings/useSettingsConfiguration.ts
@@ -69,7 +69,8 @@ export function useSettingsConfiguration() {
           text: t('common.auto-theme')
         }
       ],
-      reloadAfterSave: true
+      reloadAfterSave: true,
+      hidden: true
     }
   ])
 }
