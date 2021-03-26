96dbff9d1dd70a05ead3ed952f1d1182d6b8c1ac
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-03-26 14:21:27 +0100

    docs: more readable theme docs 📝

--- client/theme/dark.ts
+++ client/theme/dark.ts
@@ -2,55 +2,68 @@ import { Theme } from '@fluentui/react'
 
 export default {
   palette: {
-     themeDarker: '#dcdcdc',
-     themeDark: '#dcdcdc',
-     themeDarkAlt: '#dcdcdc',
-     themePrimary: '#dcdcdc',
-     themeSecondary: '#dcdcdc',
-     themeTertiary: '#dcdcdc',
-     themeLight: '#dcdcdc',
-     themeLighter: '#dcdcdc',
-     themeLighterAlt: '#dcdcdc',
-     black: '#f8f8f8', // strongest color, very light in dark theme
-     blackTranslucent40: '#dcdcdc',
-     neutralDark: '#f4f4f4',
-     neutralPrimary: '#c9d1d9', // main text color'
-     neutralPrimaryAlt: '#dadada',
-     neutralSecondary: '#c9d1d9',
-     neutralSecondaryAlt: '#dcdcdc',
-     neutralTertiary: '#c8c8c8',
-     neutralTertiaryAlt: '#6d6d6d',
-     neutralQuaternary: '#4f4f4f',
-     neutralQuaternaryAlt: '#484848',
-     neutralLight: '#3f3f3f',
-     neutralLighter: '#0d1117', // timesheet border, hover color. same as background for now
-     neutralLighterAlt: '#282828',
-     accent: '#dcdcdc',
-     white: '#0d1117', // This is the page background.
-     whiteTranslucent40: '#dcdcdc',
-     yellowDark: '#dcdcdc',
-     yellow: '#dcdcdc',
-     yellowLight: '#dcdcdc',
-     orange: '#dcdcdc',
-     orangeLight: '#dcdcdc',
-     orangeLighter: '#dcdcdc',
-     redDark: '#dcdcdc',
-     red: '#dcdcdc',
-     magentaDark: '#dcdcdc',
-     magenta: 'magenta',
-     magentaLight: '#dcdcdc',
-     purpleDark: '#dcdcdc',
-     purple: '#dcdcdc',
-     purpleLight: '#dcdcdc',
-     blueDark: '#dcdcdc',
-     blueMid: '#dcdcdc',
-     blue: 'blue',
-     blueLight: '#dcdcdc',
-     tealDark: '#dcdcdc',
-     teal: 'teal',
-     tealLight: '#dcdcdc',
-     greenDark: '#dcdcdc',
-     green: 'green',
-     greenLight: '#dcdcdc',
+    themeDarker: '#dcdcdc',
+    themeDark: '#dcdcdc',
+    themeDarkAlt: '#dcdcdc',
+    themePrimary: '#dcdcdc',
+    themeSecondary: '#dcdcdc',
+    themeTertiary: '#dcdcdc',
+    themeLight: '#dcdcdc',
+    themeLighter: '#dcdcdc',
+    themeLighterAlt: '#dcdcdc',
+    /**
+     * Strongest color, very light in dark theme
+     */
+    black: '#f8f8f8',
+    blackTranslucent40: '#dcdcdc',
+    neutralDark: '#f4f4f4',
+
+    /**
+     * Main text color'
+     */
+    neutralPrimary: '#c9d1d9',
+    neutralPrimaryAlt: '#dadada',
+    neutralSecondary: '#c9d1d9',
+    neutralSecondaryAlt: '#dcdcdc',
+    neutralTertiary: '#c8c8c8',
+    neutralTertiaryAlt: '#6d6d6d',
+    neutralQuaternary: '#4f4f4f',
+    neutralQuaternaryAlt: '#484848',
+    neutralLight: '#3f3f3f',
+    /**
+     * Timesheet border, hover color. same as background for now
+     */
+    neutralLighter: '#0d1117',
+    neutralLighterAlt: '#282828',
+    accent: '#dcdcdc',
+    /**
+     * This is the page background
+     */
+    white: '#0d1117',
+    whiteTranslucent40: '#dcdcdc',
+    yellowDark: '#dcdcdc',
+    yellow: '#dcdcdc',
+    yellowLight: '#dcdcdc',
+    orange: '#dcdcdc',
+    orangeLight: '#dcdcdc',
+    orangeLighter: '#dcdcdc',
+    redDark: '#dcdcdc',
+    red: '#dcdcdc',
+    magentaDark: '#dcdcdc',
+    magenta: 'magenta',
+    magentaLight: '#dcdcdc',
+    purpleDark: '#dcdcdc',
+    purple: '#dcdcdc',
+    purpleLight: '#dcdcdc',
+    blueDark: '#dcdcdc',
+    blueMid: '#dcdcdc',
+    blue: 'blue',
+    blueLight: '#dcdcdc',
+    tealDark: '#dcdcdc',
+    teal: 'teal',
+    tealLight: '#dcdcdc',
+    greenDark: '#dcdcdc',
+    green: 'green',
+    greenLight: '#dcdcdc',
   }
 } as Theme
