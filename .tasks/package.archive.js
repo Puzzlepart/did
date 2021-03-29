64bb4cc50b6cad26fade7db1d0bc564869e09564
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-03-29 18:59:44 +0200

    ci: testing archiving with ci 👷

---
+++ .tasks/package.archive.js
@@ -0,0 +1,56 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable unicorn/no-array-for-each */
+const fs = require('fs')
+const path = require('path')
+const archiver = require('archiver')
+const package = require('../package.json')
+const log = console.log
+
+async function run() {
+    log('Creating archive')
+    const output = fs.createWriteStream(`./${package.name}.zip`)
+    const archive = archiver('zip', {
+        zlib: { level: 9 }
+    })
+
+    output.on('close', () => {
+        log(`Archive has been finalized and the output file is ready with ${archive.pointer()} total bytes.`)
+    })
+
+    output.on('end', () => {
+        log('Data has been drained')
+    })
+
+    archive.on('warning', (error) => {
+        if (err.code === 'ENOENT') {
+            log(error)
+        } else {
+            log(error)
+            throw error
+        }
+    })
+
+    archive.on('error', (error) => {
+        log(error)
+        throw error
+    })
+
+    log('Archiving package.json...')
+    archive.file('package.json')
+
+    log('Archiving dependencies...')
+    Object.keys(package.dependencies).forEach(dep => {
+        archive.directory(`node_modules/${dep}`, `node_modules/${dep}`)
+    })
+    log('Archiving dist...')
+    archive.directory('dist', 'dist')
+
+    log('Piping output...')
+    archive.pipe(output)
+
+    log('Finalizing...')
+    await archive.finalize()
+}
+
+module.exports = run
--- .tasks/package.js
+++ .tasks/package.js
@@ -1,14 +1,15 @@
 require('dotenv').config()
 const concurrently = require('concurrently')
 const chalk = require('chalk')
-const rmdir = require('rimraf')
+const { promisify } = require('util')
+const rimraf = require('rimraf')
+const rmdir = promisify(rimraf)
 const path = require('path')
-const _ = require('underscore')
+const package_archive = require('./package.archive')
 const log = console.log
 
-const analyzeMode = _.contains(process.argv, 'analyze')
-
-log(chalk.white(`                                                              
+async function run() {
+  log(chalk.white(`                                                              
                 ddddddd                     ddddddd            
                 d:::::d   iiii              d:::::d            
                 d:::::d  i::::i             d:::::d            
@@ -23,31 +24,23 @@ log(chalk.white(`
     d::::::ddddd::::::d  i::::i d::::::ddddd::::::d            
      d::::::::::::::::d  i::::i d:::::::::::::::::d            
        dddddddddddddddd  iiiiii   ddddddddddddddddd 
-       
-       ${chalk.cyan('Watching client and server changes concurrently...')}
-
-       
-       ${analyzeMode ? chalk.magenta('[Running webpack in analyze mode]') : ''}
 
   `))
 
-const dir = path.resolve(__dirname, '../', 'server', 'public', 'js')
+  // if (process.env.NODE_ENV === 'development') {
+  // const dir = path.resolve(__dirname, '../', 'dist')
+  // log()
+  // log()
+  // log(`Cleaning directory ${chalk.cyan(dir)} 🗑️`)
+  // log()
+  // log()
+  // await rmdir(dir)
+  // }
 
-log()
-log()
-log(`Cleaning directory ${chalk.cyan(dir)} 🗑️`)
-log()
-log()
-
-let webpackCmd = 'webpack --config webpack/config.js --watch'
-
-if (_.contains(process.argv, 'analyze')) {
-  webpackCmd += ' --analyze'
+  // await concurrently([
+  //   { command: 'npm run package:client', name: 'package:client' },
+  //   { command: "npm run build:server", name: 'build:server' }
+  // ], {})
+  await package_archive()
 }
-
-rmdir(dir, () => {
-  concurrently([
-    { command: 'nodemon', name: 'server' },
-    { command: webpackCmd, name: 'client' }
-  ], {})
-})
+run()
