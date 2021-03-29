70cc62660363e4b8e159a25aa866e078af2ec682
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-03-30 00:38:36 +0200

    refactor: cleanarray just does _.filter(array, boolean) ♻️

--- client/utils/generateColumn.test.ts
+++
@@ -1,8 +0,0 @@
-import test from 'ava'
-import { generateColumn } from './generateColumn'
-
-test('generateColumn generates a default column correctly', t => {
-  const result = generateColumn('nameField', 'Name')
-  console.log(result)
-  t.is(1,1)
-})
