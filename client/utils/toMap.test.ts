d4e506300ca7b9a8095c2f61d0c0edc94b12e590
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-03-30 15:55:40 +0200

    test: test for tomap ✅

--- client/utils/toMap.test.ts
+++ client/utils/toMap.test.ts
@@ -1,3 +1,12 @@
 import test from 'ava'
+import { toMap } from './toMap'
 
-test.todo('Implement test for this later')
+test('toMap converts an object to a Map', t => {
+    const result = toMap({
+        name: 'Ole',
+        age: 38
+    })
+    t.is(result.size, 2)
+    t.is(result.get('name'), 'Ole')
+    t.is(result.get('age'), 38)
+})
