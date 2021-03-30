121473e7ff0861967868f23d0ef3cbfe5b42408d
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-03-30 16:03:28 +0200

    test: test for getsum ✅

--- client/utils/getSum.test.ts
+++ client/utils/getSum.test.ts
@@ -1,3 +1,17 @@
 import test from 'ava'
+import { getSum } from './getSum'
 
-test.todo('Implement test for this later')
+test('getSum returns the correct sum for property duration', t => {
+    const result = getSum([
+        {
+            duration: 5
+        },
+        {
+            duration: 12
+        },
+        {
+            duration: 3
+        }
+    ], 'duration')
+    t.is(result, 20)
+})
