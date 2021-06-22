30b5c39e9c56366333a662bd1bf4a50fa1bcc401
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-06-22 12:30:15 +0200

    Updated dataUpdatedCaseReducer

--- client/pages/Reports/hooks/useReportsQuery.ts
+++ client/pages/Reports/hooks/useReportsQuery.ts
@@ -1,23 +1,22 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable tsdoc/syntax */
-import { useQuery } from '@apollo/client'
+import { useLazyQuery } from '@apollo/client'
 import { useLayoutEffect } from 'react'
 import { DATA_UPDATED } from '../reducer/actions'
 import { default_query } from './useReportsQueries'
 /**
  * Hook for Reports Query.
  *
- * Using `useQuery` with and dispatches
+ * Using `useLazyQuery` and `useLayoutEffect` and dispatches
  * `DATA_UPDATED` action on query changes.
  *
  * @category Reports Hooks
  */
 export function useReportsQuery({ state, dispatch }) {
-  const query = useQuery(state.preset?.query || default_query, {
-    skip: !state.preset,
+  const [query, result] = useLazyQuery(state.preset?.query || default_query, {
     fetchPolicy: 'no-cache',
-    variables: state.preset?.variables || {},
-    pollInterval: 0
+    variables: state.preset?.variables || {}
   })
-  useLayoutEffect(() => dispatch(DATA_UPDATED({ query })), [query.loading])
+  useLayoutEffect(() => dispatch(DATA_UPDATED({ result })), [result.loading])
+  return query
 }
