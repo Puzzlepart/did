a61de82b1fcf8bfac0d43449c2ba531d0cff8a9a
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-08-12 15:15:55 +0200

    startTeamsConversation function in its own file

--- client/pages/Admin/MissingSubmissions/TeamsReminderButton/index.tsx
+++ client/pages/Admin/MissingSubmissions/TeamsReminderButton/index.tsx
@@ -1,28 +1,9 @@
-/* eslint-disable tsdoc/syntax */
 import { ActionButton } from '@fluentui/react'
 import { useAppContext } from 'AppContext'
 import React from 'react'
-import { TFunction, useTranslation } from 'react-i18next'
-import { first } from 'underscore'
-import { createTeamsConversationLink } from 'utils/createTeamsConversationLink'
+import { useTranslation } from 'react-i18next'
+import { startTeamsConversation } from './startTeamsConversation'
 import { ITeamsReminderButtonProps } from './types'
-
-const startTeamsConversation = (
-  { users, period, topic }: ITeamsReminderButtonProps,
-  t: TFunction
-) => {
-  let message = t(
-    'admin.missingSubmissions.teamsReminderMessageSinglePeriodTemplate',
-    { period: period?.name }
-  )
-  if (first(users).periods) {
-    message = t('admin.missingSubmissions.teamsReminderMessageTemplate', {
-      periods: first(users).periods.map((p) => p.name).join(', ')
-    })
-  }
-  const url = createTeamsConversationLink(users.map(u => u.email).filter(Boolean), message, topic)
-  window.open(url, '_blank')
-}
 
 export const TeamsReminderButton: React.FC<ITeamsReminderButtonProps> = (
   props
