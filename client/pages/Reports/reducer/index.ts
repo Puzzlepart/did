90431e726e5f3ce42ede3d60fac72e90acc10e39
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-11-25 12:45:27 +0100

    Reports reducer, removed actions

--- client/pages/Reports/reducer/index.ts
+++ client/pages/Reports/reducer/index.ts
@@ -3,15 +3,12 @@ import get from 'get-value'
 import _, { find } from 'underscore'
 import { IReportsState } from '../types'
 import {
-  ADD_FILTER,
+  ADD_SAVED_FILTER,
   CHANGE_QUERY,
-  CLEAR_FILTERS,
   DATA_UPDATED,
-  FILTERS_UPDATED,
-  REMOVE_SELECTED_FILTER,
+  REMOVE_SELECTED_SAVED_FILTER,
   SET_FILTER,
-  SET_GROUP_BY,
-  TOGGLE_FILTER_PANEL
+  SET_GROUP_BY
 } from './actions'
 
 /**
@@ -57,7 +54,7 @@ export default ({ initialState, queries }) =>
         state.isFiltered =
           state.subset.length !== state.data?.timeEntries?.length
       })
-      .addCase(ADD_FILTER, (state, { payload }) => {
+      .addCase(ADD_SAVED_FILTER, (state, { payload }) => {
         const newFilter: any = {
           ...current(state).filter,
           ...payload.model
@@ -68,36 +65,11 @@ export default ({ initialState, queries }) =>
         }
         state.filter = newFilter
       })
-      .addCase(REMOVE_SELECTED_FILTER, (state) => {
+      .addCase(REMOVE_SELECTED_SAVED_FILTER, (state) => {
         state.savedFilters = _.omit(state.savedFilters, state.filter.key)
         state.filter = null
         state.subset = state.data?.timeEntries
         state.isFiltered = false
-      })
-      .addCase(TOGGLE_FILTER_PANEL, (state) => {
-        state.isFiltersOpen = !state.isFiltersOpen
-      })
-      .addCase(FILTERS_UPDATED, (state, { payload }) => {
-        state.filter = {
-          key: null,
-          values: payload.filters.reduce(
-            (object, f) => ({
-              ...object,
-              [f.key]: f.selected.map((index) => index.key)
-            }),
-            {}
-          )
-        }
-        state.subset = _.filter(state.data?.timeEntries, (entry) => {
-          return (
-            _.filter(payload.filters, (f) => {
-              const selectedKeys = f.selected.map((s) => s.key)
-              return selectedKeys.includes(get(entry, f.key, { default: '' }))
-            }).length === payload.filters.length
-          )
-        })
-        state.isFiltered =
-          state.subset.length !== state.data?.timeEntries?.length
       })
       .addCase(SET_GROUP_BY, (state, { payload }) => {
         state.groupBy = payload.groupBy
@@ -108,11 +80,6 @@ export default ({ initialState, queries }) =>
           (q) => q.itemKey === payload?.itemKey
         ) as any
         state.subset = []
-      })
-      .addCase(CLEAR_FILTERS, (state) => {
-        state.filter = null
-        state.subset = state.data?.timeEntries
-        state.isFiltered = false
       })
   )
 
