b9169b4d4a33a1695b92cf35d41b220fdb263946
Ole Martin Pettersen <olemp@puzzlepart.com>
2023-02-27 13:21:34 +0100

    Improved homepage

--- client/pages/Home/useHome.ts
+++ client/pages/Home/useHome.ts
@@ -1,11 +1,16 @@
+import { useQuery } from '@apollo/client'
 import { useAppContext } from 'AppContext'
 import { useLocation } from 'react-router-dom'
+import $home from './home.gql'
 
 /**
  * Component logic for `Home`
  */
 export function useHome() {
   const { user, subscription } = useAppContext()
+  const query = useQuery($home, {
+    fetchPolicy: 'cache-first'
+  })
   const location = useLocation<{ prevPath: string }>()
   const urlSearchParameters = new URLSearchParams(document.location.search)
   const error =
@@ -13,10 +18,12 @@ export function useHome() {
     JSON.parse(atob(urlSearchParameters.get('error')))
   const redirectPage =
     user.startPage &&
-    user.startPage !== '/' &&
-    location.state?.prevPath === undefined
+      user.startPage !== '/' &&
+      location.state?.prevPath === undefined
       ? user.startPage
       : null
+  // eslint-disable-next-line no-console
+  console.log(query?.data)
   return {
     error,
     subscription,
