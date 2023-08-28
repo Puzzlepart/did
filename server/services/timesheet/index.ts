ecd0726930d7576b39b52efa2577df81701c47dd
Ole Martin Pettersen <olemp@puzzlepart.com>
2023-08-28 08:50:34 +0200

    Removed unused import

--- server/services/timesheet/index.ts
+++ server/services/timesheet/index.ts
@@ -11,7 +11,6 @@ import {
   TimesheetPeriodObject,
   VacationSummary
 } from '../../graphql/resolvers/types'
-import { toFixed } from '../../utils'
 import {
   ConfirmedPeriodsService,
   ForecastedPeriodsService,
