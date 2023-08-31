f949ff024afd6cad4d35441fcf253f7aa09cc79b
Ole Martin Pettersen <olemp@puzzlepart.com>
2023-08-31 09:22:18 +0200

    Timesheet/ActionBar

---
+++ client/pages/Timesheet/ActionBar/icons.ts
@@ -0,0 +1,40 @@
+import {
+  ArrowCircleLeft24Filled,
+  ArrowCircleLeft24Regular,
+  ArrowCircleRight24Filled,
+  ArrowCircleRight24Regular,
+  bundleIcon,
+  CalendarCancel24Filled,
+  CalendarCancel24Regular,
+  CalendarSync24Filled,
+  CalendarSync24Regular,
+  CalendarToday24Filled,
+  CalendarToday24Regular,
+  Timer24Filled,
+  Timer24Regular
+} from '@fluentui/react-icons'
+
+const CalendarToday = bundleIcon(CalendarToday24Filled, CalendarToday24Regular)
+const ArrowCircleLeft = bundleIcon(
+  ArrowCircleLeft24Filled,
+  ArrowCircleLeft24Regular
+)
+const ArrowCircleRight = bundleIcon(
+  ArrowCircleRight24Filled,
+  ArrowCircleRight24Regular
+)
+const Timer = bundleIcon(Timer24Filled, Timer24Regular)
+const CalendarCancel = bundleIcon(
+  CalendarCancel24Filled,
+  CalendarCancel24Regular
+)
+const CalendarSync = bundleIcon(CalendarSync24Filled, CalendarSync24Regular)
+
+export {
+  ArrowCircleLeft,
+  ArrowCircleRight,
+  CalendarCancel,
+  CalendarSync,
+  CalendarToday,
+  Timer
+}
--- client/pages/Timesheet/ActionBar/index.tsx
+++ client/pages/Timesheet/ActionBar/index.tsx
@@ -1,24 +1,86 @@
 /* eslint-disable unicorn/prefer-query-selector */
-import { CommandBar } from '@fluentui/react'
+import { Toolbar, ToolbarButton } from '@fluentui/react-components'
 import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { useTimesheetContext } from '../context'
+import {
+  NEXT_PERIOD,
+  PREVIOUS_PERIOD,
+  SET_DATE_RANGE
+} from '../reducer/actions'
+import { TimesheetDateRange } from '../types'
 import { DateRangePicker } from './DateRangePicker'
-import { useActionBar } from './useActionBar'
+import {
+  ArrowCircleLeft,
+  ArrowCircleRight,
+  CalendarCancel,
+  CalendarSync,
+  CalendarToday,
+  Timer
+} from './icons'
 
 /**
  * @category Timesheet
  */
 export const ActionBar = () => {
-  const { commandBarProps, showWeekPicker, toggleWeekPicker, target } =
-    useActionBar()
+  const { t } = useTranslation()
+  const { state, dispatch, onSubmitPeriod, onUnsubmitPeriod } =
+    useTimesheetContext()
 
   return (
     <div>
-      <CommandBar {...commandBarProps} />
-      <DateRangePicker
-        target={target}
-        hidden={!showWeekPicker}
-        onDismiss={toggleWeekPicker}
-      />
+      <Toolbar size='large'>
+        <ToolbarButton
+          icon={<CalendarToday />}
+          onClick={() => {
+            dispatch(
+              SET_DATE_RANGE(
+                new TimesheetDateRange(new Date(), state.dateRangeType)
+              )
+            )
+          }}
+          disabled={state.dateRange.isCurrent || !!state.loading}
+        />
+        <ToolbarButton
+          icon={<ArrowCircleLeft />}
+          onClick={() => dispatch(PREVIOUS_PERIOD())}
+          disabled={!!state.loading}
+        />
+        <ToolbarButton
+          icon={<ArrowCircleRight />}
+          onClick={() => dispatch(NEXT_PERIOD())}
+          disabled={!!state.loading}
+        />
+        <DateRangePicker />
+        <ToolbarButton
+          icon={<Timer />}
+          onClick={() => onSubmitPeriod(true)}
+          disabled={!!state.loading}
+        >
+          {t('timesheet.forecastHoursText')}
+        </ToolbarButton>
+        <ToolbarButton
+          icon={<CalendarCancel />}
+          onClick={() => onUnsubmitPeriod(true)}
+          disabled={!!state.loading}
+        >
+          {t('timesheet.unforecastHoursText')}
+        </ToolbarButton>
+        <ToolbarButton
+          icon={<CalendarSync />}
+          onClick={() => onSubmitPeriod(false)}
+          disabled={!!state.loading}
+        >
+          {t('timesheet.confirmHoursText')}
+        </ToolbarButton>
+        <ToolbarButton
+          icon={<CalendarCancel />}
+          onClick={() => onUnsubmitPeriod(false)}
+          disabled={!!state.loading}
+        >
+          {t('timesheet.unconfirmHoursText')}
+        </ToolbarButton>
+      </Toolbar>
     </div>
   )
 }
