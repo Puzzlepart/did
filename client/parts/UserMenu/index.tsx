0f97288696518c1b6767ea91eb4d5fcf4533d27b
Ole Martin Pettersen <olemp@puzzlepart.com>
2023-09-04 10:05:26 +0200

    UseMenu - updates to styling

--- client/parts/UserMenu/index.tsx
+++ client/parts/UserMenu/index.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable tsdoc/syntax */
-import { Callout, Icon, Persona, PersonaSize, useTheme } from '@fluentui/react'
+import { Icon, useTheme } from '@fluentui/react'
+import { Persona, Popover, PopoverSurface, PopoverTrigger } from '@fluentui/react-components'
 import { useAppContext } from 'AppContext'
 import { Toast, useToast } from 'components/Toast'
 import { useToggle } from 'hooks'
 import React, { FC, useEffect, useRef } from 'react'
-import { isMobile, MobileView } from 'react-device-detect'
-import FadeIn from 'react-fade-in'
+import { MobileView, isMobile } from 'react-device-detect'
 import { useTranslation } from 'react-i18next'
 import { UserFeedback } from '../UserFeedback'
 import { UserNotifications } from '../UserNotifications'
@@ -17,6 +17,7 @@ import styles from './UserMenu.module.scss'
 import { UserReports } from './UserReports'
 import { UserSettings } from './UserSettings'
 import { UserVacation } from './UserVacation'
+import { getFluentIcon } from 'utils/getFluentIcon'
 
 /**
  * @category Function Component
@@ -24,7 +25,7 @@ import { UserVacation } from './UserVacation'
 export const UserMenu: FC = () => {
   const { t } = useTranslation()
   const { user, subscription } = useAppContext()
-  const { semanticColors, palette } = useTheme()
+  const { palette } = useTheme()
   const [menuHidden, toggleMenu] = useToggle(true)
   const target = useRef(null)
   const [toast, setToast] = useToast(8000)
@@ -35,81 +36,72 @@ export const UserMenu: FC = () => {
       intent: 'success'
     })
     sessionStorage.removeItem('did_on_load_user_menu_mesage')
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
-    <>
-      <span ref={target} className={styles.root} onClick={() => toggleMenu()}>
-        <Persona
-          className={styles.user}
-          text={user.displayName}
-          secondaryText={user.mail}
-          imageUrl={user.photo?.base64}
-          size={PersonaSize.size32}
-          hidePersonaDetails={isMobile}
-        />
-        <span hidden={isMobile}>
-          <Icon
-            iconName={menuHidden ? 'ChevronDown' : 'ChevronUp'}
-            styles={{ root: { color: 'white', marginLeft: 6 } }}
-          />
-        </span>
-        <MobileView renderWithFragment={true}>
-          <NotificationIndicator />
-        </MobileView>
-      </span>
-      <Callout
-        hidden={menuHidden}
-        styles={{
-          calloutMain: {
-            background: semanticColors.menuBackground,
-            borderRadius: 6
-          }
-        }}
-        target={target?.current}
-        onDismiss={toggleMenu}
-        isBeakVisible={false}
-        gapSpace={-8}
-      >
-        <FadeIn className={styles.menu}>
-          <MenuItem
-            text={subscription.name}
-            style={{
-              padding: '0 2px 2px 12px',
-              color: palette.neutralSecondary,
-              fontSize: 10
+    <Popover withArrow={true}>
+      <PopoverTrigger>
+        <span ref={target} className={styles.root} onClick={() => toggleMenu()}>
+          <Persona
+            className={styles.user}
+            name={user.displayName}
+            secondaryText={user.mail}
+            avatar={{
+              image: {
+                src: user.photo?.base64
+              }
             }}
+            size='small'
           />
-          <UserAvatar />
-          <UserVacation />
-          <UserReports />
-          <Divider />
-          <UserSettings />
+          <span hidden={isMobile}>
+            <Icon
+              iconName={menuHidden ? 'ChevronDown' : 'ChevronUp'}
+              styles={{ root: { color: 'white', marginLeft: 6 } }}
+            />
+          </span>
           <MobileView renderWithFragment={true}>
-            <Divider />
-            <UserNotifications renderAsMenuItem={true} />
-            <Divider />
-            <UserFeedback renderAsMenuItem={true} />
+            <NotificationIndicator />
           </MobileView>
+          <Toast {...toast} />
+        </span>
+      </PopoverTrigger>
+      <PopoverSurface className={styles.menu}>
+        <MenuItem
+          text={subscription.name}
+          style={{
+            padding: '0 2px 2px 12px',
+            color: palette.neutralSecondary,
+            fontSize: 8,
+            fontWeight: 600
+          }}
+        />
+        <UserAvatar />
+        <UserVacation />
+        <UserReports />
+        <Divider />
+        <UserSettings />
+        <MobileView renderWithFragment={true}>
           <Divider />
-          <MenuItem
-            href='/auth/signout'
-            iconProps={{ iconName: 'SignOut' }}
-            text={t('common.signOutText')}
-          />
-          <MenuItem
-            style={{
-              textAlign: 'right',
-              fontSize: 10,
-              padding: 8,
-              color: 'rgb(96, 94, 92)'
-            }}
-            text={`v${process.env.VERSION}`}
-          />
-        </FadeIn>
-      </Callout>
-      <Toast {...toast} />
-    </>
+          <UserNotifications renderAsMenuItem={true} />
+          <Divider />
+          <UserFeedback renderAsMenuItem={true} />
+        </MobileView>
+        <Divider />
+        <MenuItem
+          href='/auth/signout'
+          icon={getFluentIcon('SignOut')}
+          text={t('common.signOutText')}
+        />
+        <MenuItem
+          style={{
+            textAlign: 'right',
+            fontSize: 10,
+            padding: 8,
+            color: 'rgb(96, 94, 92)'
+          }}
+          text={`v${process.env.VERSION}`}
+        />
+      </PopoverSurface>
+    </Popover>
   )
 }
