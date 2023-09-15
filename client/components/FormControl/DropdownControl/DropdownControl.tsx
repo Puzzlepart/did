cb5ef8d3a36fb13d9b7f2e26643054041c617db6
Ole Martin Pettersen <olemp@puzzlepart.com>
2023-09-15 20:10:34 +0200

    DropdownControl using @fluentui/react-components

--- client/components/FormControl/DropdownControl/DropdownControl.tsx
+++ client/components/FormControl/DropdownControl/DropdownControl.tsx
@@ -1,10 +1,11 @@
-import { Dropdown } from '@fluentui/react'
+import { Dropdown, Option } from '@fluentui/react-components'
 import React from 'react'
 import _ from 'underscore'
 import { Field } from '../Field'
 import { FormInputControlComponent } from '../types'
 import styles from './DropdownControl.module.scss'
 import { IDropdownControlProps } from './types'
+import { useDropdownControlChange } from './useDropdownControlChange'
 
 /**
  * Text field based on `<Dropdown />` from [@fluentui/react](@fluentui/react)
@@ -13,15 +14,16 @@ import { IDropdownControlProps } from './types'
  */
 export const DropdownControl: FormInputControlComponent<IDropdownControlProps> =
   (props) => {
+    const onChange = useDropdownControlChange(props)
     return (
       <Field className={DropdownControl.className} {...props}>
-        <Dropdown
-          {..._.omit(props, 'label')}
-          onChange={(_event, option) => {
-            props.model.set(props.name, option[props.setValue || 'key'])
-          }}
-          defaultSelectedKey={props.model.value(props.name) as string}
-        />
+        <Dropdown onOptionSelect={onChange}>
+          {_.map(props.values, (option, index) => (
+            <Option key={index} value={option.value}>
+              {option.text}
+            </Option>
+          ))}
+        </Dropdown>
       </Field>
     )
   }
