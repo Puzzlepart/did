d08cf82fb22e74cf06c22ee5fff4cc1db385e1c3
Ole Martin Pettersen <olemp@puzzlepart.com>
2023-09-18 15:00:54 +0200

    Improved ColumnHeader component

--- client/pages/Reports/SummaryView/ColumnHeader/ColumnHeader.tsx
+++ client/pages/Reports/SummaryView/ColumnHeader/ColumnHeader.tsx
@@ -1,22 +1,22 @@
-import { IDetailsColumnRenderTooltipProps } from '@fluentui/react'
 import { Caption2Strong, Text } from '@fluentui/react-components'
 import React from 'react'
 import { StyledComponent } from 'types'
 import styles from './ColumnHeader.module.scss'
+import { IColumnHeaderProps } from './types'
 
 /**
  * @category List
  */
-export const ColumnHeader: StyledComponent<IDetailsColumnRenderTooltipProps> = (
+export const ColumnHeader: StyledComponent<IColumnHeaderProps> = (
   props
 ) => {
   return (
     <div className={ColumnHeader.className}>
       <div className={`${styles.container} ${props.hostClassName}`}>
-        <Text weight='semibold' size={400}>
-          {props.column.name}
+        <Text weight='semibold' size={props.textSize}>
+          {props.text}
         </Text>
-        <Caption2Strong>{props.column.data?.subText}</Caption2Strong>
+        <Caption2Strong>{props.subText}</Caption2Strong>
       </div>
     </div>
   )
@@ -24,3 +24,7 @@ export const ColumnHeader: StyledComponent<IDetailsColumnRenderTooltipProps> = (
 
 ColumnHeader.displayName = 'ColumnHeader'
 ColumnHeader.className = styles.columnHeader
+ColumnHeader.defaultProps = {
+    subText: '',
+    textSize: 400
+}
--- client/pages/Reports/SummaryView/PeriodColumn/PeriodColumnTooltip/PeriodColumnTooltip.tsx
+++ client/pages/Reports/SummaryView/PeriodColumn/PeriodColumnTooltip/PeriodColumnTooltip.tsx
@@ -1,13 +1,12 @@
 import {
-  Avatar,
-  Caption2Strong,
-  Text,
-  Tooltip
+    Avatar,
+    Tooltip
 } from '@fluentui/react-components'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { StyledComponent } from 'types'
 import _ from 'underscore'
+import { ColumnHeader } from '../../ColumnHeader'
 import { CustomerHours } from './CustomerHours'
 import styles from './PeriodColumnTooltip.module.scss'
 import { TotalHours } from './TotalHours'
@@ -18,53 +17,52 @@ import { usePeriodColumnTooltip } from './usePeriodColumnTooltip'
  * @category SummaryView
  */
 export const PeriodColumnTooltip: StyledComponent<IPeriodColumnTooltipProps> = (
-  props
+    props
 ) => {
-  const { t } = useTranslation()
-  const { week, month, monthName, year, customerTotals } =
-    usePeriodColumnTooltip(props)
-  if (_.isEmpty(props.periods)) {
-    return props.children as any
-  }
-  return (
-    <Tooltip
-      withArrow={true}
-      relationship='description'
-      content={
-        <div className={PeriodColumnTooltip.className}>
-          <div className={styles.header}>
-            <div className={styles.title}>
-              <Text weight='semibold' size={500} block>
-                {t('common.periodColumnTooltipTitle', { week, month })}
-              </Text>
-              <Caption2Strong>{`${monthName} ${year}`} </Caption2Strong>
-            </div>
-            {props.user && (
-              <Avatar
-                className={styles.userAvatar}
-                name={props.user.displayName}
-                image={{
-                  src: props.user?.photo?.base64
-                }}
-                size={40}
-              />
-            )}
-          </div>
-          <div className={styles.customerTotals}>
-            {customerTotals.map(({ customer, hours }, index) => {
-              if (!customer) return null
-              return (
-                <CustomerHours key={index} customer={customer} hours={hours} />
-              )
-            })}
-          </div>
-          <TotalHours hours={props.hours.total} />
-        </div>
-      }
-    >
-      {props.children as any}
-    </Tooltip>
-  )
+    const { t } = useTranslation()
+    const { week, month, monthName, year, customerTotals } =
+        usePeriodColumnTooltip(props)
+    if (_.isEmpty(props.periods)) {
+        return props.children as any
+    }
+    return (
+        <Tooltip
+            withArrow={true}
+            relationship='description'
+            content={
+                <div className={PeriodColumnTooltip.className}>
+                    <div className={styles.header}>
+                        <ColumnHeader
+                            textSize={500}
+                            text={t('common.periodColumnTooltipTitle', { week, month })}
+                            subText={`${monthName} ${year}`}
+                        />
+                        {props.user && (
+                            <Avatar
+                                className={styles.userAvatar}
+                                name={props.user.displayName}
+                                image={{
+                                    src: props.user?.photo?.base64
+                                }}
+                                size={40}
+                            />
+                        )}
+                    </div>
+                    <div className={styles.customerTotals}>
+                        {customerTotals.map(({ customer, hours }, index) => {
+                            if (!customer) return null
+                            return (
+                                <CustomerHours key={index} customer={customer} hours={hours} />
+                            )
+                        })}
+                    </div>
+                    <TotalHours hours={props.hours.total} />
+                </div>
+            }
+        >
+            {props.children as any}
+        </Tooltip>
+    )
 }
 
 PeriodColumnTooltip.displayName = 'PeriodColumnTooltip'
