fcf897e92cad9818d8ad4d4aacd0ba4e2f3b963f
Ole Martin Pettersen <olemp@puzzlepart.com>
2023-10-07 15:32:28 +0200

    VersionInfo: fix for the case where displayVersionDetailsToooltip is false

--- client/parts/UserMenu/VersionInfo/VersionInfo.tsx
+++ client/parts/UserMenu/VersionInfo/VersionInfo.tsx
@@ -1,28 +1,34 @@
-import { Link, Tooltip } from '@fluentui/react-components'
+import { Caption2Strong, Link, Tooltip, mergeClasses } from '@fluentui/react-components'
 import React from 'react'
 import { StyledComponent } from 'types'
 import styles from './VersionInfo.module.scss'
 
 export const VersionInfo: StyledComponent = () => {
-    return (
-        <Tooltip
-            relationship='description'
-            content={DISPLAY_VERSION_DETAILS === '1' && (
-                <div className={styles.versionInfoTooltip}>
-                    <div>
-                        <b className={styles.infoLabel}>Commit:</b><Link href={COMMIT_URL} target='_blank'><span>{COMMIT_HASH}</span></Link>
+    const displayVersionDetailsToooltip = DISPLAY_VERSION_DETAILS === '1'
+    return displayVersionDetailsToooltip
+        ? (
+            <Tooltip
+                withArrow
+                relationship='description'
+                content={(
+                    <div className={styles.versionInfoTooltip}>
+                        <div>
+                            <b className={styles.infoLabel}>Commit:</b><Link href={COMMIT_URL} target='_blank'><span>{COMMIT_HASH}</span></Link>
+                        </div>
+                        <div>
+                            <b className={styles.infoLabel}>Branch:</b><Link href={BRANCH_URL} target='_blank'><span>{BRANCH}</span></Link>
+                        </div>
+                        <div>
+                            <b className={styles.infoLabel}>Last commit:</b><span>{new Date(LAST_COMMIT_DATETIME).toLocaleString()}</span>
+                        </div>
                     </div>
-                    <div>
-                        <b className={styles.infoLabel}>Branch:</b><Link href={BRANCH_URL} target='_blank'><span>{BRANCH}</span></Link>
-                    </div>
-                    <div>
-                        <b className={styles.infoLabel}>Last commit:</b><span>{new Date(LAST_COMMIT_DATETIME).toLocaleString()}</span>
-                    </div>
-                </div>
-            )}>
-            <div className={VersionInfo.className}>{`v${VERSION}`}</div>
-        </Tooltip>
-    )
+                )}>
+                <Caption2Strong className={mergeClasses(VersionInfo.className, styles.detailsAvailable)}>{`v${VERSION}`}</Caption2Strong>
+            </Tooltip>
+        )
+        : (
+            <Caption2Strong className={VersionInfo.className}>{`v${VERSION}`}</Caption2Strong>
+        )
 }
 
 VersionInfo.displayName = 'VersionInfo'
