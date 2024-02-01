2a7978d847d75ca3edb345896c3cfb68f0ccc532
Ole Martin Pettersen <olemp@puzzlepart.com>
2024-02-01 14:02:33 +0100

    WeekStatusObject

--- server/graphql/resolvers/timesheet/types/WeekStatusObject.ts
+++ server/graphql/resolvers/timesheet/types/WeekStatusObject.ts
@@ -1,19 +1,40 @@
 import 'reflect-metadata'
-import { Field, Int, ObjectType } from 'type-graphql'
+import { Field, Float, Int, ObjectType } from 'type-graphql'
 
 /**
+ * Return object for the `weekStatus` query
+ * 
  * @category GraphQL ObjectType
  */
 @ObjectType({
-    description: 'Object for WeekStatus'
+    description: 'Return object for the weekStatus query'
 })
 export class WeekStatusObject {
+    /**
+     * User ID
+     */
     @Field()
     userId: string
 
-    @Field()
-    submitted: boolean
-
+    /**
+     * Submit status
+     * 
+     * `0` - Not submitted
+     * `1` - Partially submitted (the week is split between months and one of the months is submitted)
+     * `2` - Fully submitted
+     */
     @Field(() => Int)
+    submitStatus: number
+
+    /**
+     * Total hours for the week
+     */
+    @Field(() => Float)
     hours: number
+
+    /**
+     * Is the week split between months?
+     */
+    @Field()
+    isWeekSplit: boolean
 }
