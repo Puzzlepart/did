372ff1cca1c5639c94de8339208cc23690fbbd38
Ole Martin Pettersen <olemp@puzzlepart.com>
2024-04-26 09:30:02 +0200

    adjust: just a small adjustment to useuserpickerquery 🔧 [skip-ci]

--- client/components/FormControl/UserPickerControl/UserPicker/useUserPickerQuery.ts
+++ client/components/FormControl/UserPickerControl/UserPicker/useUserPickerQuery.ts
@@ -1,4 +1,4 @@
-import { useQuery } from '@apollo/client'
+import { WatchQueryFetchPolicy, useQuery } from '@apollo/client'
 import _ from 'lodash'
 import { User } from 'types'
 import $users from './users.gql'
@@ -7,12 +7,16 @@ import { IUserPickerState } from './types'
 
 /**
  * Custom hook for handling user picker query.
+ *
+ * @param setState The state setter function.
+ * @param fetchPolicy The fetch policy for the query which defaults to `cache-first`.
  */
 export function useUserPickerQuery(
-  setState: (newState: React.SetStateAction<IUserPickerState>) => void
+  setState: (newState: React.SetStateAction<IUserPickerState>) => void,
+  fetchPolicy: WatchQueryFetchPolicy = 'cache-first'
 ) {
   const query = useQuery<{ users: User[] }>($users, {
-    fetchPolicy: 'cache-and-network'
+    fetchPolicy
   })
 
   useEffect(() => {
