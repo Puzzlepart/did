0d2310fc089526f3406e55560019c9f1ef00e613
Ole Martin Pettersen <olemp@puzzlepart.com>
2024-04-26 09:07:50 +0200

    fix: minor fix in filtervalues for projectlist 🐛 [skip-ci]

--- client/pages/Projects/ProjectList/ProjectList.tsx
+++ client/pages/Projects/ProjectList/ProjectList.tsx
@@ -41,7 +41,7 @@ export const ProjectList: TabComponent<IProjectListProps> = (props) => {
           showInactive.value
             ? {}
             : {
-                '!inactive': true
+                inactive: false
               }
         }
       />
