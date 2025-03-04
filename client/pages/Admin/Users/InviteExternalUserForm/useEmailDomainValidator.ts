c6cf33109fdb2ea2cd07cb16f6d91ec03e4edcd5
Ole Martin Pettersen <olemp@puzzlepart.com>
2025-03-04 17:49:16 +0100

    refactor: fixed useemaildomainvalidator ♻️

--- client/pages/Admin/Users/InviteExternalUserForm/useEmailDomainValidator.ts
+++ client/pages/Admin/Users/InviteExternalUserForm/useEmailDomainValidator.ts
@@ -3,7 +3,17 @@ import { ValidatorFunction } from 'components'
 import { useTranslation } from 'react-i18next'
 import _ from 'lodash'
 
-export function useEmailDomainValidator() {
+/**
+ * Custom hook that returns a validator function to check if an email domain is allowed.
+ * 
+ * The validator function checks if the domain part of the email is included in the list of allowed domains
+ * specified in the subscription settings under 'security.domainRestrictionExternal'.
+ * 
+ * @returns A function that takes an email string as input and returns:
+ * - `null` if the email domain is valid or if there are no domain restrictions.
+ * - An array containing an error message and the string 'error' if the email domain is not valid.
+ */
+export function useEmailDomainValidator(): ValidatorFunction {
   const { t } = useTranslation()
   const domainRestrictionExternal = useSubscriptionSettings<string[]>(
     'security.domainRestrictionExternal'
@@ -20,10 +30,10 @@ export function useEmailDomainValidator() {
     return isValidDomain
       ? null
       : [
-          t('common.invalidEmailDomainValidation', {
-            domain: emailDomain
-          }),
-          'error'
-        ]
-  }) as ValidatorFunction
+        t('common.invalidEmailDomainValidation', {
+          domain: emailDomain
+        }),
+        'error'
+      ]
+  }) as ValidatorFunction<string>
 }
