6b89860012b575bec2e5e6bdc34d0eb7c3e35263
Ole Martin Pettersen <olemp@puzzlepart.com>
2025-03-05 15:41:09 +0100

    fix: resetting selection etc 🐛

--- client/components/FormControl/DropdownControl/useDropdownControlChange.ts
+++ client/components/FormControl/DropdownControl/useDropdownControlChange.ts
@@ -1,12 +1,26 @@
 import { DropdownProps } from '@fluentui/react-components'
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 import { IDropdownControlProps } from './types'
+import _ from 'lodash'
 
 export function useDropdownControlChange(props: IDropdownControlProps) {
-  return useCallback<DropdownProps['onOptionSelect']>((_event, data) => {
+  const onChange = useCallback<DropdownProps['onOptionSelect']>((_event, data) => {
     const value = props.options?.preTransformValue
       ? props.options.preTransformValue(data)
       : data.optionValue
     props.model.set(props.name, value)
   }, [])
+
+  useEffect(() => {
+    if (!props.selectFirstOption) return
+    if (_.isEmpty(props.values)) return
+    onChange(null, {
+      selectedOptions: [],
+      optionValue: props.values[0].value,
+      optionText: props.values[0].text
+    })
+  }, [props.selectFirstOption])
+
+
+  return onChange
 }
