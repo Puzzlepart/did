dda5821c86a8ee8f6f2e070611752d217517d851
Ole Martin Pettersen <olemp@puzzlepart.com>
2025-03-20 15:40:06 +0100

    adjust: reload button not available when a period is confirmed 🔧

--- client/pages/Timesheet/ActionBar/ReloadButton/ReloadButton.tsx
+++ client/pages/Timesheet/ActionBar/ReloadButton/ReloadButton.tsx
@@ -8,13 +8,15 @@ import { useTimesheetContext } from 'pages/Timesheet/context'
 export const ReloadButton: FC<IReloadButtonProps> = (props) => {
   const { t } = useTranslation()
   const context = useTimesheetContext()
+  if (context.state?.selectedPeriod?.isConfirmed || Boolean(context.state.loading)) {
+    return null
+  }
   return (
     <ToolbarButton
+      hidden
       icon={icon(props.iconName)}
-      onClick={() => {
-        context.refetch({ cache: props.cache })
-      }}
-    >
+      title={t('timesheet.reloadButtonTooltip')}
+      onClick={() => context.refetch({ cache: props.cache })}>
       {t('timesheet.reloadButtonText')}
     </ToolbarButton>
   )
