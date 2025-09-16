2623782cd040cd7ce5e43674c9b74ad231ffb288
Carl Joakim Damsleth <kim@damsleth.com>
2025-09-16 15:39:18 +0200

    fix: fixed ava testrunner config error causing all tests to fail 🐛 [skip-ci]

--- client/pages/Admin/Users/UserForm/useAdUserSearch.ts
+++ client/pages/Admin/Users/UserForm/useAdUserSearch.ts
@@ -7,17 +7,20 @@ export function useAdUserSearch() {
   const [searchResults, setSearchResults] = useState<ActiveDirectoryUser[]>([])
   const [searchLoading, setSearchLoading] = useState(false)
   const timeoutRef = useRef<ReturnType<typeof setTimeout>>()
-  
-  const [searchActiveDirectoryUsers] = useLazyQuery($searchActiveDirectoryUsers, {
-    onCompleted: (data) => {
-      setSearchResults(data.searchActiveDirectoryUsers || [])
-      setSearchLoading(false)
-    },
-    onError: () => {
-      setSearchResults([])
-      setSearchLoading(false)
+
+  const [searchActiveDirectoryUsers] = useLazyQuery(
+    $searchActiveDirectoryUsers,
+    {
+      onCompleted: (data) => {
+        setSearchResults(data.searchActiveDirectoryUsers || [])
+        setSearchLoading(false)
+      },
+      onError: () => {
+        setSearchResults([])
+        setSearchLoading(false)
+      }
     }
-  })
+  )
 
   const performSearch = useCallback(
     async (search: string) => {
