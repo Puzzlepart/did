a6b380e034591517250585b7a69313ab2eb7c338
Carl Joakim Damsleth <kim@damsleth.com>
2026-01-13 12:33:28 +0100

    ♻️ refactor: Update component types to use Fluent UI v9 ButtonProps and enhance IColumn interface

--- client/pages/Admin/WeekStatus/LockWeekButton/types.ts
+++ client/pages/Admin/WeekStatus/LockWeekButton/types.ts
@@ -1,6 +1,6 @@
-import { IButtonProps } from '@fluentui/react'
+import { ButtonProps } from '@fluentui/react-components'
 import { IMissingSubmissionPeriod } from '../types'
 
-export interface ILockWeekButtonProps extends IButtonProps {
+export interface ILockWeekButtonProps extends ButtonProps {
   period: IMissingSubmissionPeriod
 }
--- client/pages/Admin/WeekStatus/TeamsReminderButton/types.ts
+++ client/pages/Admin/WeekStatus/TeamsReminderButton/types.ts
@@ -1,9 +1,8 @@
-import { IButtonProps } from '@fluentui/react'
+import { ButtonProps } from '@fluentui/react-components'
 import { IDatePeriod } from '../../../../../shared/utils/DateObject'
 import { IMissingSubmissionUser } from '../MissingSubmissionUser'
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface ITeamsReminderButtonProps extends IButtonProps {
+export interface ITeamsReminderButtonProps extends ButtonProps {
   users: IMissingSubmissionUser[]
   period?: IDatePeriod
   topic?: string
